1fea82e5e7333430740ae7e44bd74bea42d16a02
dyigitpolat <yigit@u.nus.edu>
2022-09-14 21:32:14 +0800

    test utils

---
+++ src/mimarsinan/test/xor_test/xor_test_utils.py
@@ -0,0 +1,42 @@
+from mimarsinan.test.test_models.simple_mlp import *
+
+import numpy as np
+import torch
+
+def get_xor_train_data():
+    train_x = torch.Tensor([
+        [0., 0.],
+        [0., 1.],
+        [1., 0.],
+        [1., 1.]])
+    train_y = torch.Tensor([
+        [1.,0.],
+        [0.,1.],
+        [0.,1.],
+        [1.,0.]
+        ])
+    
+    return (train_x, train_y)
+
+def train_xor_model():
+    device = "cpu"
+    num_epochs = 500
+    
+    train_x, train_y = get_xor_train_data()
+
+    loss = 1.0
+    ann = None
+    while(loss > 0.01):
+        del ann
+        ann = SimpleMLP()
+        optimizer = torch.optim.Adam(ann.parameters(), lr = 0.01)
+        for _ in range(num_epochs):
+            ann.train()
+            train_y.to(device)
+            outputs = ann.forward(train_x)
+            loss = nn.MSELoss()(outputs.cpu(), train_y)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+        
+    return ann
