c7270045df8c7d5994e357c13fafdbd51f9f5364
dyigitpolat <yigit@u.nus.edu>
2022-09-16 20:09:35 +0800

    nni test

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from mimarsinan.test.xor_test.xor_test import *
 from mimarsinan.test.mnist_test.mnist_test import *
+from mimarsinan.test.mnist_nni_test.test_mnist_nni import *
 
-test_xor()
+# test_xor()
 test_mnist()
+# test_mnist_nni()
