f90a048e7841a19c1b3ae7fa90450044f6261fb1
dyigitpolat <yigit@u.nus.edu>
2022-10-06 21:14:22 +0800

    main function update

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from mimarsinan.test.xor_test.xor_test import *
 from mimarsinan.test.mnist_test.mnist_test import *
 from mimarsinan.test.mnist_nni_test.test_mnist_nni import *
+from mimarsinan.test.mnist_ntk_nni_test.test_mnist_ntk_nni import *
 
 # test_xor()
-test_mnist()
+# test_mnist()
 # test_mnist_nni()
+test_mnist_ntk_nni()
