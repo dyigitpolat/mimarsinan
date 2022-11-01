a82743a91e1492544851ab2161b19f3c0d17f455
dyigitpolat <yigit@u.nus.edu>
2022-11-01 11:23:32 +0800

    cifar 10 test

---
+++ src/mimarsinan/test/cifar10_test/cifar10_test.py
@@ -0,0 +1,59 @@
+from mimarsinan.models.simple_mlp import *
+from mimarsinan.test.cifar10_test.cifar10_test_utils import *
+
+from mimarsinan.mapping.simple_mlp_mapper import *
+from mimarsinan.chip_simulation.compile_nevresim import *
+from mimarsinan.chip_simulation.execute_nevresim import *
+from mimarsinan.code_generation.generate_main import *
+from mimarsinan.test.test_utils import *
+
+import torch
+
+def test_cifar10():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
+    cifar10_input_size = 32*32*3
+    cifar10_output_size = 10
+    inner_mlp_width = 256
+    inner_mlp_count = 3
+    epochs = 20
+
+    ann_model = SimpleMLP(
+        inner_mlp_width, 
+        inner_mlp_count, 
+        cifar10_input_size, 
+        cifar10_output_size)
+
+    print("Training model...")
+    train_on_cifar10(ann_model, device, epochs)
+
+    generated_files_path = "../generated/cifar10/"
+    simulation_length = 200
+    input_count = 100
+
+    _, test_loader = get_cifar10_data(1)
+
+    print("Mapping trained model to chip...")
+    chip = simple_mlp_to_chip(ann_model, leak=0.0)
+
+    print("Saving trained weights and chip generation code...")
+    save_inputs_to_files(generated_files_path, test_loader, input_count)
+    save_weights_and_chip_code(chip, generated_files_path)
+
+    print("Generating main function code...")
+    generate_main_function(generated_files_path, input_count, simulation_length)
+
+    print("Compiling nevresim for mapped chip...")
+    simulator_filename = \
+        compile_simulator(generated_files_path, "../nevresim/")
+    print("Compilation outcome:", simulator_filename)
+
+    print("Executing simulator...")
+    chip_output = execute_simulator(simulator_filename)
+
+    print("Evaluating simulator output...")
+    _, test_loader = get_cifar10_data(1)
+    accuracy = evaluate_chip_output(chip_output, test_loader, cifar10_output_size)
+
+    print("SNN accuracy on CIFAR10 is:", accuracy*100, "%")
+    print("CIFAR10 test done.")
---
+++ src/mimarsinan/test/cifar10_test/cifar10_test_utils.py
@@ -0,0 +1,60 @@
+import torch.nn as nn
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+datasets_path = "../datasets"
+
+def get_cifar10_data(batch_size=1):
+    train_dataset = torchvision.datasets.CIFAR10(
+        root=datasets_path, train=True, download=True,
+        transform=transforms.ToTensor())
+
+    test_set = torchvision.datasets.CIFAR10(
+        root=datasets_path, train=False, download=True,
+        transform=transforms.ToTensor())
+                            
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=batch_size, shuffle=True, num_workers=0)
+    test_loader = torch.utils.data.DataLoader(
+        test_set, batch_size=batch_size, shuffle=False, num_workers=0)
+
+    return train_loader, test_loader
+
+def train_on_cifar10_for_one_epoch(ann, device, optimizer, train_loader, epoch):
+    print("Training epoch:", epoch)
+    for (x, y) in train_loader:
+        optimizer.zero_grad()
+        ann.train()
+        y.to(device)
+        outputs = ann.forward(x)
+        loss = nn.CrossEntropyLoss()(outputs.cpu(), y)
+        loss.backward()
+        optimizer.step()
+
+def train_on_cifar10(ann, device, epochs):
+    train_loader, _ = get_cifar10_data(4096)
+    optimizer = torch.optim.Adam(ann.parameters(), lr = 0.001)
+    
+    for epoch in range(epochs):
+        train_on_cifar10_for_one_epoch(
+            ann, device, optimizer, train_loader, epoch)
+
+        if(epoch % max(epochs // 10, 1) == 0):
+            correct, total = test_on_cifar10(ann, device)
+            print(correct, '/', total)
+
+def test_on_cifar10(ann, device):
+    total = 0
+    correct = 0
+    with torch.no_grad():
+        _, test_loader = get_cifar10_data(4096)
+        for (x, y) in test_loader:
+            y.to(device)
+            outputs = ann.forward(x)
+            _, predicted = outputs.cpu().max(1)
+            total += float(y.size(0))
+            correct += float(predicted.eq(y).sum().item())
+    
+    return correct, total
