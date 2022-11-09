6311f91efa0d106470a6beb929dc13b865f3d3dc
dyigitpolat <yigit@u.nus.edu>
2022-11-09 20:19:26 +0800

    cifar 10 nni

---
+++ src/mimarsinan/test/cifar10_nni_test/test_cifar10_nni.py
@@ -0,0 +1,29 @@
+from mimarsinan.models.simple_mlp import *
+from mimarsinan.test.mnist_test.mnist_test_utils import *
+from mimarsinan.test.test_utils import *
+
+from nni.experiment import Experiment
+
+def test_mnist_nni():
+    experiment = Experiment('local')
+
+    search_space = {
+        'inner_mlp_count': {'_type': 'quniform', '_value': [1, 5, 1]},
+        'inner_mlp_width': {'_type': 'choice', '_value': [
+            64, 128, 256, 512, 1024]}
+    }
+
+    experiment.config.trial_command = \
+        'python mimarsinan/test/mnist_nni_test/mnist_nni_worker.py'
+    experiment.config.trial_code_directory = '.'
+    experiment.config.search_space = search_space
+    experiment.config.tuner.name = 'TPE'
+    experiment.config.tuner.class_args['optimize_mode'] = 'maximize'
+    experiment.config.max_trial_number = 40
+    experiment.config.trial_concurrency = 10
+
+    experiment.run(8082, wait_completion=True)
+
+    for trial in experiment.export_data():
+        print(trial.parameter, "accuracy: ", trial.value)
+    
