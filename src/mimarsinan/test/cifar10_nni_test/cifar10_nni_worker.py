9f4488241d0d3333248fec85f4a564631c86a42e
dyigitpolat <yigit@u.nus.edu>
2022-11-13 01:59:31 +0800

    parametric search with nni on cifar 10 using ntk

--- src/mimarsinan/test/cifar10_nni_test/cifar10_nni_worker.py
+++ src/mimarsinan/test/cifar10_nni_test/cifar10_nni_worker.py
@@ -1,35 +1,57 @@
 import sys
 sys.path.append(r'./')
 
-from mimarsinan.models.simple_mlp import *
+from mimarsinan.models.simple_mlp_mixer import *
 from mimarsinan.test.test_utils import *
 from mimarsinan.test.cifar10_test.cifar10_test_utils import *
+from mimarsinan.model_evaluation.te_nas_utils import *
 
 import torch
 import nni
 
+def get_ntk(model, input_loader):
+    return get_ntk_n(input_loader, [model], num_batch=1)[0]
+    
 def cifar10_nni_worker():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    
-    cifar10_input_size = 32*32*3
+
+    cifar10_h = 32
+    cifar10_w = 32
+    cifar10_c = 3
     cifar10_output_size = 10
 
+    epochs = 20
+    
     parameters = nni.get_next_parameter()
-    inner_mlp_width = int(parameters['inner_mlp_width'])
-    inner_mlp_count = int(parameters['inner_mlp_count'])
-    epochs = int((10*inner_mlp_count*(inner_mlp_width//64))**0.5)
 
-    ann_model = SimpleMLP(
-        inner_mlp_width, 
-        inner_mlp_count, 
-        cifar10_input_size, 
+    region_borders_x = get_region_borders(
+        int(parameters['patch_cols']), 
+        int(parameters['patch_center_x']), 
+        int(parameters['patch_lensing_exp_x']))
+
+    region_borders_y = get_region_borders(
+        int(parameters['patch_rows']), 
+        int(parameters['patch_center_y']), 
+        int(parameters['patch_lensing_exp_y']))
+        
+    ann_model = SimpleMLPMixer(
+        int(parameters['patch_rows']), int(parameters['patch_cols']),
+        int(parameters['patch_features']),
+        int(parameters['patch_channels']),
+        int(parameters['mixer_features']),
+        int(parameters['inner_mlp_width']),
+        int(parameters['inner_mlp_count']),
+        region_borders_x,
+        region_borders_y,
+        cifar10_h,cifar10_w,cifar10_c, 
         cifar10_output_size)
 
-    print("Training model...")
-    train_on_cifar10(ann_model, device, epochs)
-    correct, total = test_on_cifar10(ann_model, device)
+    print("Evaluating model...")
+    train_loader, _ = get_cifar10_data(200)
+    ntk = get_ntk(ann_model, train_loader)
+    print(f"NTK:{ntk}")
 
-    nni.report_final_result(correct / total)
+    nni.report_final_result(ntk)
 
 if __name__ == "__main__":
     cifar10_nni_worker()
--- src/mimarsinan/test/cifar10_nni_test/test_cifar10_nni.py
+++ src/mimarsinan/test/cifar10_nni_test/test_cifar10_nni.py
@@ -1,29 +1,44 @@
-from mimarsinan.models.simple_mlp import *
-from mimarsinan.test.mnist_test.mnist_test_utils import *
+from mimarsinan.models.simple_mlp_mixer import *
+from mimarsinan.test.cifar10_test.cifar10_test_utils import *
 from mimarsinan.test.test_utils import *
 
 from nni.experiment import Experiment
 
-def test_mnist_nni():
+def test_cifar10_nni():
     experiment = Experiment('local')
 
     search_space = {
+
+        'patch_cols': {'_type': 'quniform', '_value': [2, 8, 1]},
+        'patch_rows': {'_type': 'quniform', '_value': [2, 8, 1]},
+        'patch_features': {'_type': 'choice', '_value': [
+            16, 32, 48, 64, 96, 128, 192, 256]},
+        'patch_channels': {'_type': 'quniform', '_value': [1, 16, 1]},
+        'mixer_features': {'_type': 'choice', '_value': [
+            16, 32, 48, 64, 96, 128, 192, 256]},
         'inner_mlp_count': {'_type': 'quniform', '_value': [1, 5, 1]},
         'inner_mlp_width': {'_type': 'choice', '_value': [
-            64, 128, 256, 512, 1024]}
+            16, 32, 48, 64, 96, 128, 192, 256]},
+        'patch_center_x': {'_type': 'uniform', '_value': [-0.15, 0.15]},
+        'patch_center_y': {'_type': 'uniform', '_value': [-0.15, 0.15]},
+        'patch_lensing_exp_x': {'_type': 'uniform', '_value': [0.5, 2.0]},
+        'patch_lensing_exp_y': {'_type': 'uniform', '_value': [0.5, 2.0]}
     }
 
     experiment.config.trial_command = \
-        'python mimarsinan/test/mnist_nni_test/mnist_nni_worker.py'
+        'python mimarsinan/test/cifar10_nni_test/cifar10_nni_worker.py'
     experiment.config.trial_code_directory = '.'
     experiment.config.search_space = search_space
     experiment.config.tuner.name = 'TPE'
-    experiment.config.tuner.class_args['optimize_mode'] = 'maximize'
-    experiment.config.max_trial_number = 40
+    experiment.config.tuner.class_args['optimize_mode'] = 'minimize'
+    experiment.config.max_trial_number = 100
     experiment.config.trial_concurrency = 10
 
     experiment.run(8082, wait_completion=True)
 
-    for trial in experiment.export_data():
-        print(trial.parameter, "accuracy: ", trial.value)
+    trials = experiment.export_data()
+    for trial in trials:
+        print(trial.parameter, "ntk: ", trial.value)
+
+    print("best: ", min([(t.value, t.parameter) for t in trials]))
     
