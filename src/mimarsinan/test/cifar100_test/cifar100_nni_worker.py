44bdc0e6fffc05bc49889a17858254e5056d1e61
yigit polat <dyigit96@gmail.com>
2023-01-16 15:46:14 +0800

    parametric model refactoring

--- src/mimarsinan/test/cifar100_test/cifar100_nni_worker.py
+++ src/mimarsinan/test/cifar100_test/cifar100_nni_worker.py
@@ -1,13 +1,17 @@
 import sys
 sys.path.append(r'./')
 
-from mimarsinan.models.simple_mlp_mixer import *
+from mimarsinan.models.omihub_mlp_mixer import *
+from mimarsinan.models.ensemble_mlp_mixer import *
 from mimarsinan.test.test_utils import *
 from mimarsinan.test.cifar100_test.cifar100_test_utils import *
 from mimarsinan.model_evaluation.te_nas_utils import *
 
 import torch
 import nni
+
+def get_number_of_mlp_mixers():
+    return 2
 
 def get_ntk(model, input_loader):
     return get_ntk_n(input_loader, [model], num_batch=1)[0]
@@ -19,33 +23,15 @@ def cifar100_nni_worker():
     cifar100_output_size = 100
     
     parameters = nni.get_next_parameter()
-
-    region_borders_x = get_region_borders(
-        int(parameters['patch_cols']), 
-        int(parameters['patch_center_x']), 
-        int(parameters['patch_lensing_exp_x']),
-        cifar100_w)
-
-    region_borders_y = get_region_borders(
-        int(parameters['patch_rows']), 
-        int(parameters['patch_center_y']), 
-        int(parameters['patch_lensing_exp_y']),
-        cifar100_h)
-        
-    ann_model = SimpleMLPMixer(
-        int(parameters['patch_rows']), int(parameters['patch_cols']),
-        int(parameters['features_per_patch']),
-        int(parameters['mixer_channels']),
-        int(parameters['mixer_features']),
-        int(parameters['inner_mlp_width']),
-        int(parameters['inner_mlp_count']),
-        region_borders_x,
-        region_borders_y,
-        cifar100_h,cifar100_w,cifar100_c, 
-        cifar100_output_size)
+    #parameters = {'patch_size0': 2, 'hidden_size0': 192, 'hidden_c0': 256, 'hidden_s0': 16, 'num_layers0': 32, 'patch_size1': 16, 'hidden_size1': 192, 'hidden_c1': 192, 'hidden_s1': 64, 'num_layers1': 8}
+    print(parameters)
+    ann_model = EnsembleMLPMixer(
+        get_parameter_dict_list(parameters, get_number_of_mlp_mixers()), 
+        cifar100_h, cifar100_w, cifar100_c, cifar100_output_size
+    )
 
     print("Evaluating model...")
-    train_loader, _ = get_cifar100_data(200)
+    train_loader, _ = get_dataloaders(Args())
     ntk = get_ntk(ann_model, train_loader)
     print(f"NTK:{ntk}")
 
