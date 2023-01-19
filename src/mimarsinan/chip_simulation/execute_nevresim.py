eb4b9ddfddce4865a6f1e59462bcd684b16892b0
yigit polat <dyigit96@gmail.com>
2023-01-19 17:44:21 +0800

    add parallel execution support

--- src/mimarsinan/chip_simulation/execute_nevresim.py
+++ src/mimarsinan/chip_simulation/execute_nevresim.py
@@ -1,12 +1,22 @@
 import subprocess
 
-def execute_simulator(simulator_filename):
-    cmd = ["./{}".format(simulator_filename)]
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+def execute_simulator(simulator_filename, input_count, num_proc = 4):
+    pipes = []
+    for i in range(num_proc):
+        start = i * input_count // num_proc
+        end = (i + 1) * input_count // num_proc
+        if(i == num_proc - 1):
+            end = input_count
 
-    
-    line = p.stdout.readline()
-    output_values = [int(val) for val in line.split()]
-    p.wait()
+        cmd = ["./{}".format(simulator_filename), str(start), str(end)]
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+        pipes.append(p)
+
+    output_values = []
+    for p in pipes:
+        line = p.stdout.readline()
+        for val in line.split():
+            output_values.append(int(val)) 
+        p.wait()
 
     return output_values
--- src/mimarsinan/code_generation/main_cpp_template.py
+++ src/mimarsinan/code_generation/main_cpp_template.py
@@ -2,10 +2,11 @@ main_cpp_template = \
 """
 #include "generate_chip.hpp"
 #include "_tests/all.hpp"
+#include <cstdlib>
 
 namespace nevresim::tests {{
     
-void test_main()
+void test_main(int start, int end)
 {{
     static constinit auto chip = 
         generate_chip<SpikingCompute>();
@@ -15,7 +16,7 @@ void test_main()
     load_weights(
         chip, "{0}weights/chip_weights.txt");
 
-    for(int idx = 0; idx < {1}; ++idx)
+    for(int idx = start; idx < end; ++idx)
     {{
         auto [input, target] = load_input_n("{0}inputs/", idx);
         auto buffer = chip.template execute<exec>(input);
@@ -30,8 +31,9 @@ void test_main()
 
 }}
 
-int main()
+int main(int argc, char** argv)
 {{
-    nevresim::tests::test_main();
+    nevresim::tests::test_main(
+        std::atoi(argv[1]), std::atoi(argv[2]));
 }}
 """
