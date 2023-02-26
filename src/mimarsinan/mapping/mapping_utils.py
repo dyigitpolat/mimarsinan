b25d098f87a29bcee67de7f396682b582673669d
yigit polat <dyigit96@gmail.com>
2023-02-26 16:33:03 +0800

    remove legacy def

--- src/mimarsinan/mapping/mapping_utils.py
+++ src/mimarsinan/mapping/mapping_utils.py
@@ -53,35 +53,6 @@ def generate_core_weights(
     
     return Core(neurons)
 
-def generate_core_weights_legacy(
-    neurons_count, axons_count, weight_tensor, outs, 
-    thresh, bias_tensor = None, quantize = False):
-    
-    max_w = weight_tensor.max().item()
-    min_w = weight_tensor.min().item()
-    if(quantize):
-        if(max_w == 0): max_w = 1.0
-        thresh = round(q_max * thresh / max_w)
-
-    neurons: list[Neuron] = []
-    for idx in range(neurons_count):
-        if(idx < outs):
-            neuron_ws = [ 
-                do_quantize(w.item(), max_w, min_w, quantize) \
-                for w in weight_tensor[idx] ]
-
-            for _ in range(axons_count - weight_tensor[idx].shape[0]):
-                neuron_ws.append(int(0))
-        else:
-            neuron_ws = [int(0) for _ in range(axons_count)]
-
-        bias = 0.0
-        if(bias_tensor is not None) and (idx < outs): bias = bias_tensor[idx]
-        
-        neurons.append(Neuron(neuron_ws, thresh, bias))
-    
-    return Core(neurons)
-
 def generate_core_connection_info(
     axons_count, ins, core, is_input_core):
     axon_sources = [SpikeSource(core, i, is_input_core) for i in range(ins)]
