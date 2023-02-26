b445e6e9914a83667421d0c55546c045e3f75a0d
yigit polat <dyigit96@gmail.com>
2023-02-26 16:27:16 +0800

    remove debug run

--- src/mimarsinan/test/mnist_test/mnist_test.py
+++ src/mimarsinan/test/mnist_test/mnist_test.py
@@ -46,43 +46,13 @@ def test_mnist():
     print("Tuning model with CQ and weight quantization...")
     train_on_mnist_quantized(cq_ann_model, device, cq_quantize_epochs)
 
-    #######################################
-
     print("Mapping trained model to chip...")
     chip = simple_mlp_to_chip(cq_ann_model, leak=0, quantize=True, weight_type=int)
 
-    print("Saving trained weights and chip generation code...")
+    print("Saving input data to files...")
     save_inputs_to_files(generated_files_path, test_loader, input_count)
-    save_weights_and_chip_code(chip, generated_files_path)
-
-    print("Generating main function code...")
-    generate_main_function(
-        generated_files_path, input_count, mnist_output_size, simulation_length,
-        main_cpp_template, get_config("Deterministic", "Novena", "int"))
-
-    print("Compiling nevresim for mapped chip...")
-    simulator_filename = \
-        compile_simulator(generated_files_path, "../nevresim/")
-    print("Compilation outcome:", simulator_filename)
-
-    print("Executing simulator...")
-    start_time = time.time()
-    chip_output = execute_simulator(simulator_filename, input_count, num_proc=50)
-    end_time = time.time()
-    print("Simulation time:", end_time - start_time)
-
-    print("Evaluating simulator output...")
-    _, test_loader = get_mnist_data(1)
-    accuracy = evaluate_chip_output(chip_output, test_loader, mnist_output_size, verbose=True)
-    print("SNN accuracy on MNIST is:", accuracy*100, "%")
-
-    #######################################
-
-    print("Mapping trained model to chip...")
-    chip = simple_mlp_to_chip_v2(cq_ann_model, leak=0, quantize=True, weight_type=int)
 
     print("Saving trained weights and chip generation code...")
-    save_inputs_to_files(generated_files_path, test_loader, input_count)
     save_weights_and_chip_code(chip, generated_files_path)
 
     print("Generating main function code...")
