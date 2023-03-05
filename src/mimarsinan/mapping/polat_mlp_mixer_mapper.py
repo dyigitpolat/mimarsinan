3ba1d924a624b50996c881351a5120c34566537a
yigit polat <dyigit96@gmail.com>
2023-03-06 03:00:34 +0800

    novena compatible mlp mixer model

---
+++ src/mimarsinan/mapping/polat_mlp_mixer_mapper.py
@@ -0,0 +1,79 @@
+from mimarsinan.code_generation.cpp_chip_model import *
+from mimarsinan.mapping.mapping_utils import *
+from mimarsinan.models.omihub_mlp_mixer import *
+    
+def polat_mlp_mixer_to_chip(
+    omihub_mlp_mixer_model: MLPMixer,
+    leak = 0.0,
+    quantize = False,
+    weight_type = float):
+    model = omihub_mlp_mixer_model.cpu()
+
+    neurons_per_core = 256
+    axons_per_core = 256
+
+    in_channels = model.img_dim_c
+    in_height = model.img_dim_h
+    in_width = model.img_dim_w
+
+    input_size = in_channels * in_height * in_width
+    
+    mapping = Mapping()
+    mapping.neurons_per_core = neurons_per_core
+    mapping.axons_per_core = axons_per_core
+
+    input_shape = (in_channels, in_height, in_width)
+    layer_sources = prepare_input_sources(input_shape)
+
+    fc_layers = []
+    fc_layers.append(PatchEmbeddingLayer()) # input
+    for i in range(model.num_layers):
+        fc_layers.append(
+            model.mixer_layers[i].mlp1.fc1)
+        fc_layers.append(
+            model.mixer_layers[i].mlp1.fc2)
+        fc_layers.append(
+            model.mixer_layers[i].mlp1.ln)
+        
+        fc_layers.append(
+            model.mixer_layers[i].mlp2.fc1)
+        fc_layers.append(
+            model.mixer_layers[i].mlp2.fc2)
+        fc_layers.append(
+            model.mixer_layers[i].mlp2.ln)
+    
+    fc_layers.append(AvgPoolLayer())
+    fc_layers.append(model.clf)
+
+    fuse_layers(fc_layers)
+        
+    layer_sources_list = []
+    for layer in fc_layers:
+        layer_sources_list.append(layer_sources)
+
+        if isinstance(layer, nn.Conv1d):
+            layer_sources = map_conv1d(mapping, layer_sources, layer, quantize)
+        elif isinstance(layer, nn.Linear):
+            layer_sources = map_linear(mapping, layer_sources, layer, quantize)
+        elif isinstance(layer, AvgPoolLayer):
+            layer_sources = map_avg_pool(mapping, layer_sources, quantize)
+        elif isinstance(layer, AddOp):
+            layer_sources = map_add_op(mapping, 
+                layer_sources_list[layer.source_idx_a], 
+                layer_sources_list[layer.source_idx_b])
+        elif isinstance(layer, PatchEmbeddingLayer):
+            layer_sources = map_patch_embedding(mapping, layer_sources, model.patch_emb[0], quantize)
+            
+    output_list = []
+    for source in layer_sources.flatten():
+        output_list.append(source)
+    
+    chip = ChipModel(axons_per_core, neurons_per_core, len(mapping.cores), 
+        input_size, len(output_list), leak, mapping.connections, output_list, mapping.cores, weight_type)
+
+    chip.load_from_json(chip.get_chip_json()) # sanity check
+    return chip    
+
+def export_json_to_file(chip, filename):
+    with open(filename, 'w') as f:
+        f.write(chip.get_chip_json())
--- src/mimarsinan/test/cifar10_test/cifar10_test.py
+++ src/mimarsinan/test/cifar10_test/cifar10_test.py
@@ -1,22 +1,128 @@
 from mimarsinan.test.cifar10_test.cifar10_test_utils import *
 
-from mimarsinan.models.omihub_mlp_mixer import *
 from mimarsinan.chip_simulation.compile_nevresim import *
 from mimarsinan.chip_simulation.execute_nevresim import *
 from mimarsinan.code_generation.generate_main import *
 from mimarsinan.test.test_utils import *
 
-import torch
-import json
+from mimarsinan.models.polat_mlp_mixer import *
+from mimarsinan.mapping.polat_mlp_mixer_mapper import *
 
+import torch
+import time
+
+def almost_equal(a, b, epsilon=0.001):
+    eq1 = abs(a - b) < epsilon
+    eq2 = True
+    if(b != 0.0 and b != -0.0 and a != 0.0 and a != -0.0):
+        eq2 = abs(1.0 - a/b) < 0.1
+
+    return eq1 and eq2
+
+def get_ann_model(args):
+    return PolatMLPMixer(
+        in_channels=3,
+        img_size=32, 
+        patch_size=8, 
+        hidden_size=64, 
+        hidden_s=32, 
+        hidden_c=128, 
+        num_layers=2, 
+        num_classes=10, 
+        drop_p=0.).to(args.device)
+
+def train_model(args):
+    print("Training model...")
+    wandb.login()
+
+    experiment_name = f"{args.seed}_{args.model}_{args.dataset}_{args.optimizer}_{args.scheduler}"
+    if args.autoaugment:
+        experiment_name += "_aa"
+    if args.clip_grad:
+        experiment_name += f"_cg{args.clip_grad}"
+    if args.off_act:
+        experiment_name += f"_noact"
+    if args.cutmix_prob>0.:
+        experiment_name += f'_cm'
+    if args.is_cls_token:
+        experiment_name += f"_cls"
+
+    ann_model = get_ann_model(args)
+    
+    with wandb.init(project='mlp_mixer_1000_run', config=args, name=experiment_name):
+        #train_dl, test_dl = get_dataloaders(args)
+        #prepare_containing_directory("../saved_models/")
+        ann_model.load_state_dict(torch.load("../saved_models/model.state_dict"))
+        #trainer = Trainer(ann_model, args)
+        #trainer.fit(train_dl, test_dl)
+        #torch.save(ann_model.state_dict(), f"../saved_models/model.state_dict")
+
+    return ann_model
+    
 def test_cifar10():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(device)
+
+    args = Args()
+    args.dataset = "c10"
+    args.batch_size = 128
+    args.epochs = 50
+    args.device = torch.device("cuda")
     
-    epochs = 40
-    ann_model = MLPMixer()
+    #ann_model = train_model(args)
+    ann_model = get_ann_model(args)
+    #ann_model.load_state_dict(torch.load("../saved_models/model.state_dict"))
 
-    print("Training model...")
-    train_on_cifar10(ann_model, device, epochs)
-    test_on_cifar10(ann_model, device, epochs)
+    print("---")
+    generated_files_path = "../generated/cifar10/"
 
-    print("CIFAR10 test done.")
+    print("Forward pass against test input...")
+
+    input_count = 1
+    test_input = torch.randn(input_count, 3, 32, 32)
+    out_size = 10
+    test_loader = [(test_input, torch.ones(1,out_size))]
+    ann_model(test_input.to(device))
+
+    #_, test_loader = get_cifar10_data(1)
+
+    print("Mapping trained model to chip...")
+    chip = polat_mlp_mixer_to_chip(ann_model, leak=0, quantize=False, weight_type=float)
+
+    print("Saving trained weights and chip generation code...")
+    save_inputs_to_files(generated_files_path, test_loader, input_count)
+    save_weights_and_chip_code(chip, generated_files_path)
+
+    print("Generating main function code...")
+    generate_main_function_for_real_valued_exec(generated_files_path)
+
+    print("Compiling nevresim for mapped chip...")
+    simulator_filename = \
+        compile_simulator(generated_files_path, "../nevresim/")
+    print("Compilation outcome:", simulator_filename)
+
+    print("Executing simulator...")
+    start_time = time.time()
+    chip_output = execute_simulator(simulator_filename, input_count, num_proc=50)
+    end_time = time.time()
+    print("Simulation time:", end_time - start_time)
+
+    tot = 0
+    inc = 0
+    layer_debug_tensor = ann_model.debug.detach().cpu().numpy().flatten()
+    for i in range(len(chip_output)):
+        if(not almost_equal(chip_output[i], layer_debug_tensor[i])):
+            inc +=1
+        tot += 1
+    
+    print("inc", inc)
+    print("tot", tot)
+
+    print(chip_output)
+    print(layer_debug_tensor)
+    
+    print("Evaluating simulator output...")
+    accuracy = evaluate_chip_output(chip_output, test_loader, out_size, verbose=True)
+    print("SNN accuracy on CIFAR-10 is:", accuracy*100, "%")
+
+    print("cifar10 test done.")
