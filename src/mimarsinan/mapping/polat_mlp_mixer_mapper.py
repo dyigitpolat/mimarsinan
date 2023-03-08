11f6a68ba831fce80c8515ecfbdb13a850bfc8e2
yigit polat <dyigit96@gmail.com>
2023-03-08 17:09:15 +0800

    hardcore mapping and refactored quantization

--- src/mimarsinan/mapping/polat_mlp_mixer_mapper.py
+++ src/mimarsinan/mapping/polat_mlp_mixer_mapper.py
@@ -1,5 +1,6 @@
 from mimarsinan.code_generation.cpp_chip_model import *
 from mimarsinan.mapping.mapping_utils import *
+from mimarsinan.mapping.weight_quantization import *
 from mimarsinan.models.omihub_mlp_mixer import *
     
 def polat_mlp_mixer_to_chip(
@@ -52,24 +53,46 @@ def polat_mlp_mixer_to_chip(
         layer_sources_list.append(layer_sources)
 
         if isinstance(layer, nn.Conv1d):
-            layer_sources = map_conv1d(mapping, layer_sources, layer, quantize)
+            layer_sources = map_conv1d(mapping, layer_sources, layer)
         elif isinstance(layer, nn.Linear):
-            layer_sources = map_linear(mapping, layer_sources, layer, quantize)
+            layer_sources = map_linear(mapping, layer_sources, layer)
         elif isinstance(layer, AvgPoolLayer):
-            layer_sources = map_avg_pool(mapping, layer_sources, quantize)
+            layer_sources = map_avg_pool(mapping, layer_sources)
         elif isinstance(layer, AddOp):
             layer_sources = map_add_op(mapping, 
                 layer_sources_list[layer.source_idx_a], 
                 layer_sources_list[layer.source_idx_b])
         elif isinstance(layer, PatchEmbeddingLayer):
-            layer_sources = map_patch_embedding(mapping, layer_sources, model.patch_emb[0], quantize)
+            layer_sources = map_patch_embedding(mapping, layer_sources, model.patch_emb[0])
             
     output_list = []
     for source in layer_sources.flatten():
         output_list.append(source)
     
-    chip = ChipModel(axons_per_core, neurons_per_core, len(mapping.cores), 
-        input_size, len(output_list), leak, mapping.connections, output_list, mapping.cores, weight_type)
+    if quantize:
+        quantize_softcores(mapping.soft_cores, bits=4)
+
+    hardcore_mapping = HardCoreMapping(axons_per_core, neurons_per_core)
+    hardcore_mapping.map(mapping.soft_cores, output_list)
+    
+    hardcores = [
+        generate_core_weights(
+            neurons_per_core, 
+            axons_per_core, 
+            hardcore.core_matrix.transpose(),
+            neurons_per_core,
+            hardcore.threshold)
+        for hardcore in hardcore_mapping.hardcores
+    ]
+
+    hardcore_connections = \
+        [ Connection(hardcore.axon_sources) for hardcore in hardcore_mapping.hardcores ]
+        
+    chip = ChipModel(
+        axons_per_core, neurons_per_core, len(hardcores), input_size,
+        len(output_list), leak, hardcore_connections, output_list, hardcores, 
+        weight_type
+    )
 
     chip.load_from_json(chip.get_chip_json()) # sanity check
     return chip    
---
+++ src/mimarsinan/mapping/weight_quantization.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+class WeightQuantization:
+    def __init__(self, bits):
+        self.q_min = -( 2 ** (bits - 1) )
+        self.q_max = ( 2 ** (bits - 1) ) - 1
+
+    def quantize_weight_(self, w, min_w, max_w):
+        if(w > 0):
+            if(max_w == 0): return 0
+            return round((self.q_max * w) / max_w)
+        else:
+            if(min_w == 0): return 0
+            return round((self.q_min * w) / min_w)
+
+    def quantize(self, weights):
+        max_w = weights.max().item()
+        min_w = weights.min().item()
+        return np.array([
+            [self.quantize_weight_(w, min_w, max_w) for w in weights.flatten()]
+        ]).reshape(weights.shape)
+    
+    def calculate_threshold(self, weights):
+        max_w = weights.max()
+        if(max_w == 0): max_w = 1.0
+        return round((self.q_max * 1.0) / max_w)
+
+def quantize_softcores(softcores, bits):
+    quantizer = WeightQuantization(bits)
+
+    for softcore in softcores:
+        softcore.threshold = quantizer.calculate_threshold(softcore.core_matrix)
+        softcore.core_matrix = quantizer.quantize(softcore.core_matrix)
+        
+    return softcores
