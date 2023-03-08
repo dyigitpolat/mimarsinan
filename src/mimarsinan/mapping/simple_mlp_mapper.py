43f1a9c09d8767000c14e8ec8a75f7ac3cb9bc1e
yigit polat <dyigit96@gmail.com>
2023-03-08 17:36:59 +0800

    refactor mapping

--- src/mimarsinan/mapping/simple_mlp_mapper.py
+++ src/mimarsinan/mapping/simple_mlp_mapper.py
@@ -10,15 +10,11 @@ simple_mlp_model: SimpleMLP,
     weight_type = float):
     model = simple_mlp_model.cpu()
 
-    neurons_per_core = 256
-    axons_per_core = 784
+    mapping = Mapping()
+    mapping.max_neurons = 256
+    mapping.max_axons = 784
 
     input_size = model.layers[0].weight.size(1)
-
-    mapping = Mapping()
-    mapping.neurons_per_core = neurons_per_core
-    mapping.axons_per_core = axons_per_core
-
     fc_layers = []
     for layer in model.layers:
         fc_layers.append(layer)
@@ -40,40 +36,16 @@ simple_mlp_model: SimpleMLP,
                 layer_sources_list[layer.source_idx_a], 
                 layer_sources_list[layer.source_idx_b])
         elif isinstance(layer, PatchEmbeddingLayer):
-            layer_sources = map_patch_embedding(mapping, layer_sources, model.patch_emb[0])
+            layer_sources = map_patch_embedding(mapping, layer_sources, layer.layer)
             
     output_list = []
     for source in layer_sources.flatten():
         output_list.append(source)
-    
-    if quantize:
-        quantize_softcores(mapping.soft_cores, bits=4)
 
-    hardcore_mapping = HardCoreMapping(axons_per_core, neurons_per_core)
-    hardcore_mapping.map(mapping.soft_cores, output_list)
-
-    hardcores = [
-        generate_core_weights(
-            neurons_per_core, 
-            axons_per_core, 
-            hardcore.core_matrix.transpose(),
-            neurons_per_core,
-            hardcore.threshold)
-        for hardcore in hardcore_mapping.hardcores
-    ]
-
-    hardcore_connections = \
-        [ Connection(hardcore.axon_sources) for hardcore in hardcore_mapping.hardcores ]
-        
-    chip = ChipModel(
-        axons_per_core, neurons_per_core, len(hardcores), input_size,
-        len(output_list), leak, hardcore_connections, output_list, hardcores, 
-        weight_type
-    )
-
-    chip.load_from_json(chip.get_chip_json()) # sanity check
-    
-    return chip    
+    return to_chip(
+        input_size, output_list, 
+        mapping, mapping.max_axons, mapping.max_neurons,
+        leak, quantize, weight_type)
 
 def export_json_to_file(chip, filename):
     with open(filename, 'w') as f:
