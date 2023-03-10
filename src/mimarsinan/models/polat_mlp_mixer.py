93e8e0887a63d9e7cb9d737c4c8b146abc9c0035
yigit polat <dyigit96@gmail.com>
2023-03-10 23:30:49 +0800

    support cq in polat mlp mixer

--- src/mimarsinan/models/polat_mlp_mixer.py
+++ src/mimarsinan/models/polat_mlp_mixer.py
@@ -17,6 +17,41 @@ import torchvision.transforms as transforms
 
 from mimarsinan.models.layers import * 
 
+class Conv2d_WS(nn.Conv2d):
+
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1,
+                 padding=0, dilation=1, groups=1, bias=True):
+        super(Conv2d_WS, self).__init__(in_channels, out_channels, kernel_size, stride,
+                 padding, dilation, groups, bias)
+
+    def forward(self, x):
+        weight = self.weight
+        weight_mean = weight.mean(dim=1, keepdim=True).mean(dim=2,
+                                  keepdim=True).mean(dim=3, keepdim=True)
+        weight = weight - weight_mean
+        std = weight.view(weight.size(0), -1).std(dim=1).view(-1, 1, 1, 1) + 1e-5
+        weight = weight / std.expand_as(weight)
+        return F.conv2d(x, weight, self.bias, self.stride,
+                        self.padding, self.dilation, self.groups)
+class SoftQuantize(nn.Module):
+    def __init__(self, Tq):
+        super(SoftQuantize, self).__init__()
+        self.Tq = Tq
+    
+    def forward(self, x, alpha=4.5):
+        h = 1.0 / self.Tq
+        w = 1.0 / self.Tq
+        a = torch.tensor(alpha)
+
+        output = x
+        output = torch.clamp(output, 0.0, 1.0)
+        output = h * (
+            0.5 * (1.0/torch.tanh(a/2)) * 
+            torch.tanh(a * ((output/w-torch.floor(output/w))-0.5)) + 
+            0.5 + torch.floor(output/w))
+        
+        #output = torch.clamp(output, 0.0, 1.0)
+        return output
 class PolatMLPMixer(nn.Module):
     def __init__(self,in_channels=3,img_size=32, patch_size=4, hidden_size=512, hidden_s=256, hidden_c=2048, num_layers=8, num_classes=10, drop_p=0.):
         super(PolatMLPMixer, self).__init__()
@@ -28,10 +63,12 @@ class PolatMLPMixer(nn.Module):
         self.img_dim_c = in_channels
         self.num_classes = num_classes
 
+        self.cq_layer = nn.Identity()
+
         self.patch_emb = nn.Sequential(
-            nn.Conv2d(in_channels, hidden_size ,kernel_size=patch_size, stride=patch_size, bias=False),
+            Conv2d_WS(in_channels, hidden_size ,kernel_size=patch_size, stride=patch_size, bias=False),
             Rearrange('b d h w -> b (h w) d'), 
-            nn.ReLU()
+            nn.LeakyReLU()
         )
 
         self.num_layers = num_layers
@@ -45,13 +82,36 @@ class PolatMLPMixer(nn.Module):
         self.clf = nn.Linear(hidden_size, num_classes, bias=False)
         self.norm = Normalizer()
         self.debug = None
+    
+    def enable_cq(self, Tq):
+        self.cq_layer = SoftQuantize(Tq)
+        for layer in self.mixer_layers:
+            layer.enable_cq(Tq)
+    
+    def disable_cq(self):
+        self.cq_layer = nn.Identity()
+        for layer in self.mixer_layers:
+            layer.disable_cq()
 
     def forward(self, x):
-        out = self.patch_emb(x)
+        out = self.cq_layer(x)
+        out = self.patch_emb(out)
+        out = torch.clamp(out, -0.05, 1.05)
+        out = self.cq_layer(out)
+
         out = self.mixer_layers(out)
         out = out.mean(dim=1)
+
+        out = nn.LeakyReLU()(out)
+        out = torch.clamp(out, -0.05, 1.05)
+        out = self.cq_layer(out)
+
         out = self.clf(out)
-        out = nn.ReLU()(out)
+
+        out = nn.LeakyReLU()(out)
+        out = torch.clamp(out, -0.05, 1.05)
+        out = self.cq_layer(out)
+
         self.debug = out
 
         return out
@@ -61,6 +121,14 @@ class MixerLayer(nn.Module):
         super(MixerLayer, self).__init__()
         self.mlp1 = MLP1(num_patches, hidden_s, hidden_size, drop_p)
         self.mlp2 = MLP2(hidden_size, hidden_c, drop_p)
+
+    def enable_cq(self, Tq):
+        self.mlp1.enable_cq(Tq)
+        self.mlp2.enable_cq(Tq)
+    
+    def disable_cq(self):
+        self.mlp1.disable_cq()
+        self.mlp2.disable_cq()
 
     def forward(self, x):
         out = self.mlp1(x)
@@ -76,15 +144,28 @@ class MLP1(nn.Module):
         self.do1 = nn.Dropout(p=drop_p)
         self.fc2 = nn.Conv1d(hidden_s, num_patches, kernel_size=1, bias=False)
         self.do2 = nn.Dropout(p=drop_p)
-        self.act = nn.ReLU()
+        self.act = nn.LeakyReLU()
+
+        self.cq_layer = nn.Identity()
+
+    def enable_cq(self, Tq):
+        self.cq_layer = SoftQuantize(Tq)
+    
+    def disable_cq(self):
+        self.cq_layer = nn.Identity()
+    
     def forward(self, x):
         out = self.fc1(x)
         out = self.act(out)
+        out = torch.clamp(out, -0.05, 1.05)
+        out = self.cq_layer(out)
         out = self.do1(out)
 
         out = self.fc2(out)
         out = self.ln(out)
         out = self.act(out)
+        out = torch.clamp(out, -0.05, 1.05)
+        out = self.cq_layer(out)
         out = self.do2(out)
         
         return out
@@ -97,16 +178,28 @@ class MLP2(nn.Module):
         self.do1 = nn.Dropout(p=drop_p)
         self.fc2 = nn.Linear(hidden_c, hidden_size, bias=False)
         self.do2 = nn.Dropout(p=drop_p)
-        self.act = nn.ReLU()
+        self.act = nn.LeakyReLU()
+
+        self.cq_layer = nn.Identity()
+
+    def enable_cq(self, Tq):
+        self.cq_layer = SoftQuantize(Tq)
+    
+    def disable_cq(self):
+        self.cq_layer = nn.Identity()
 
     def forward(self, x):
         out = self.fc1(x)
         out = self.act(out)
+        out = torch.clamp(out, -0.05, 1.05)
+        out = self.cq_layer(out)
         out = self.do1(out)
 
         out = self.fc2(out)
         out = self.ln(out)
         out = self.act(out)
+        out = torch.clamp(out, -0.05, 1.05)
+        out = self.cq_layer(out)
         out = self.do2(out)
 
         return out
