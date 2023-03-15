9fee0974b861557c208c75bea0ae08c2705e46c4
yigit polat <dyigit96@gmail.com>
2023-03-15 20:30:55 +0800

    fix cyclic dependency issue in chip delay

--- src/mimarsinan/mapping/chip_delay.py
+++ src/mimarsinan/mapping/chip_delay.py
@@ -2,27 +2,35 @@ class ChipDelay:
     def __init__(self, mapping):
         self.mapping = mapping
         self.memo = {}
+        self.visited = set()
 
-    def get_delay_for(self, source):
-        if source.core_ in self.memo: return self.memo[source.core_]
-        if source.is_input_: 
-            self.memo[source.core_] = 0
+    def get_delay_for(self, source_core_idx, depth=0):
+        if source_core_idx in self.memo: return self.memo[source_core_idx]
+        if source_core_idx in self.visited: return 0
+        
+        if source_core_idx < 0:
+            self.memo[source_core_idx] = 0
             return 0
-        if source.is_off_: 
-            self.memo[source.core_] = 0
-            return 0
-        if source.is_always_on_: 
-            self.memo[source.core_] = 0
-            return 0
+        
+        assert source_core_idx >= 0
+        axon_sources = self.mapping.cores[source_core_idx].axon_sources
 
-        axon_sources = self.mapping.cores[source.core_].axon_sources
-
+        source_cores = set()
+        for source in axon_sources:
+            source_cores.add(source.core_)
+        
+        self.visited.add(source_core_idx)
         result = 1 + max([
-            self.get_delay_for(source) for source in axon_sources])
+            self.get_delay_for(core, depth+1) for core in source_cores])
+        self.visited.remove(source_core_idx)
 
-        self.memo[source.core_] = result
+        self.memo[source_core_idx] = result
         return result
         
     def calculate(self):
+        self.memo = {}
+        self.visited = set()
+        print("Calculating chip delay...")
+        print("Total cores:", len(self.mapping.cores))
         return max([
-            self.get_delay_for(source) for source in self.mapping.output_sources])
+            self.get_delay_for(source.core_) for source in self.mapping.output_sources])
