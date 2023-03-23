4de95f67dbf5b7cd3cad76b22bf7727f6fdac073
yigit polat <dyigit96@gmail.com>
2023-03-23 22:18:48 +0800

    remove ws, move soft quantize to layers.py

--- src/mimarsinan/models/polat_mlp_mixer.py
+++ src/mimarsinan/models/polat_mlp_mixer.py
@@ -3,57 +3,16 @@ import torch.nn as nn
 import torch.nn.functional as F
 from einops.layers.torch import Rearrange
 
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
-import wandb
-import warmup_scheduler
-import numpy as np
-
-import torchvision
-import torchvision.transforms as transforms
 
 from mimarsinan.models.layers import * 
 
-class Conv2d_WS(nn.Conv2d):
 
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1,
-                 padding=0, dilation=1, groups=1, bias=True):
-        super(Conv2d_WS, self).__init__(in_channels, out_channels, kernel_size, stride,
-                 padding, dilation, groups, bias)
-
-    def forward(self, x):
-        weight = self.weight
-        weight_mean = weight.mean(dim=1, keepdim=True).mean(dim=2,
-                                  keepdim=True).mean(dim=3, keepdim=True)
-        weight = weight - weight_mean
-        std = weight.view(weight.size(0), -1).std(dim=1).view(-1, 1, 1, 1) + 1e-5
-        weight = weight / std.expand_as(weight)
-        self.weight.data = weight.data
-        return F.conv2d(x, weight, self.bias, self.stride,
-                        self.padding, self.dilation, self.groups)
-class SoftQuantize(nn.Module):
-    def __init__(self, Tq):
-        super(SoftQuantize, self).__init__()
-        self.Tq = Tq
-    
-    def forward(self, x, alpha=4.5):
-        h = 1.0 / self.Tq
-        w = 1.0 / self.Tq
-        a = torch.tensor(alpha)
-
-        output = x
-        output = torch.clamp(output, 0.0, 1.0)
-        output = h * (
-            0.5 * (1.0/torch.tanh(a/2)) * 
-            torch.tanh(a * ((output/w-torch.floor(output/w))-0.5)) + 
-            0.5 + torch.floor(output/w))
-        
-        #output = torch.clamp(output, 0.0, 1.0)
-        return output
 class PolatMLPMixer(nn.Module):
+    polat_activation = nn.LeakyReLU()
+
     def __init__(self,in_channels=3,img_size=32, patch_size=4, hidden_size=512, hidden_s=256, hidden_c=2048, num_layers=8, num_classes=10, drop_p=0.):
         super(PolatMLPMixer, self).__init__()
         
@@ -65,10 +24,9 @@ class PolatMLPMixer(nn.Module):
         self.num_classes = num_classes
 
         self.patch_emb = nn.Sequential(
-            #Conv2d_WS(in_channels, hidden_size ,kernel_size=patch_size, stride=patch_size, bias=True),
             nn.Conv2d(in_channels, hidden_size ,kernel_size=patch_size, stride=patch_size, bias=True),
             Rearrange('b d h w -> b (h w) d'), 
-            nn.LeakyReLU()
+            PolatMLPMixer.polat_activation
         )
 
         self.num_layers = num_layers
@@ -88,10 +46,10 @@ class PolatMLPMixer(nn.Module):
         out = self.mixer_layers(out)
         
         out = out.mean(dim=1)
-        out = nn.LeakyReLU()(out)
+        out = PolatMLPMixer.polat_activation(out)
 
         out = self.clf(out)
-        out = nn.LeakyReLU()(out)
+        out = PolatMLPMixer.polat_activation(out)
 
         self.debug = out
 
@@ -117,7 +75,7 @@ class MLP1(nn.Module):
         self.fc2 = nn.Conv1d(hidden_s, num_patches, kernel_size=1, bias=True)
         self.ln = nn.Identity() #nn.BatchNorm1d(num_patches)
         self.do2 = nn.Dropout(p=drop_p)
-        self.act = nn.LeakyReLU()
+        self.act = PolatMLPMixer.polat_activation
     
     def forward(self, x):
         out = self.fc1(x)
@@ -139,7 +97,7 @@ class MLP2(nn.Module):
         self.fc2 = nn.Linear(hidden_c, hidden_size, bias=True)
         self.ln = WokeBatchNorm1d(hidden_size)
         self.do2 = nn.Dropout(p=drop_p)
-        self.act = nn.LeakyReLU()
+        self.act = PolatMLPMixer.polat_activation
 
     def forward(self, x):
         out = self.fc1(x)
