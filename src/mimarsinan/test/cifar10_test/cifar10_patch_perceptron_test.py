ee4d3b0d0ea4eaa5d1758d3b178f3dc97c1a280f
yigit polat <dyigit96@gmail.com>
2023-03-30 16:48:28 +0800

    support cifar10 workflow with patched perceptrons

---
+++ src/mimarsinan/test/cifar10_test/cifar10_patch_perceptron_test.py
@@ -0,0 +1,188 @@
+from mimarsinan.models.perceptron_flow import *
+from mimarsinan.test.test_utils import *
+from mimarsinan.test.cifar10_test.cifar10_test_utils import *
+from mimarsinan.transformations.weight_quantization import *
+from mimarsinan.transformations.weight_clipping import *
+from mimarsinan.chip_simulation.compile_nevresim import *
+from mimarsinan.chip_simulation.execute_nevresim import *
+from mimarsinan.code_generation.generate_main import *
+from mimarsinan.transformations.chip_quantization import *
+from mimarsinan.visualization.hardcore_visualization import *
+
+import time
+
+cifar10_patched_perceptron_test_clipping_rate = 0.01
+
+def clip_model_weights(model, clipping_rate=cifar10_patched_perceptron_test_clipping_rate):
+    clipper = SoftTensorClipping(clipping_rate)
+    for param in model.parameters():
+        param.data = clipper.get_clipped_weights(param.data)
+
+def quantize_model(model, bits=4):
+    quantizer = TensorQuantization(bits)
+    for param in model.parameters():
+        param.data = quantizer.quantize(param.data)
+
+def clip_and_quantize_model(model, bits=4, clipping_rate=cifar10_patched_perceptron_test_clipping_rate):
+    clip_model_weights(model, clipping_rate)
+    quantize_model(model, bits)
+
+def test_cifar10_patched_perceptron():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
+    cifar10_input_shape = (3, 32, 32)
+    cifar10_output_size = 10
+    Tq = 30
+
+    pretrain_epochs = 48
+    cycles = 10
+    cycle_epochs = 4
+    cq_only_epochs = 12
+    cq_quantize_epochs = 48
+
+    perceptron_flow = PatchedPerceptronFlow(
+        cifar10_input_shape, cifar10_output_size,
+        240, 240, 8, 8)
+
+    print("Pretraining model...")
+    lr = 0.01
+    train_with_weight_trasformation(
+        model=perceptron_flow, 
+        device=device,
+        train_dataloader=get_cifar10_data(500)[0],
+        test_dataloader=get_cifar10_data(50000)[1],
+        weight_transformation=lambda x: x,
+        epochs=pretrain_epochs,
+        lr=lr)
+    lr *= 0.1
+
+    print("Fusing normalization...")
+    perceptron_flow.fuse_normalization()
+
+    print("Tuning model with CQ leaky clamp...")
+    for i in range(cycles):
+        clamp_leak = math.sin((i * (1.0/cycles)))**0.5
+        print("  Cycle:", i+1, "/", cycles)
+        print("  Clamp leak:", clamp_leak)
+        perceptron_flow.set_activation(CQ_Activation_LeakyClamp(Tq, clamp_leak))
+        train_with_weight_trasformation(
+            model=perceptron_flow, 
+            device=device,
+            train_dataloader=get_cifar10_data(5000)[0],
+            test_dataloader=get_cifar10_data(50000)[1],
+            weight_transformation=clip_model_weights,
+            epochs=cycle_epochs,
+            lr=lr)
+        lr = lr * 0.9
+
+    print("Tuning model with CQ only...")
+    perceptron_flow.set_activation(CQ_Activation(Tq))
+    lr *= 0.5
+    train_with_weight_trasformation(
+            model=perceptron_flow, 
+            device=device,
+            train_dataloader=get_cifar10_data(5000)[0],
+            test_dataloader=get_cifar10_data(50000)[1],
+            weight_transformation=clip_model_weights,
+            epochs=cq_only_epochs,
+            lr=lr)
+
+    print("Tuning model with CQ and weight quantization...")
+    perceptron_flow.set_activation(CQ_Activation(Tq))
+    lr *= 0.5
+    train_with_weight_trasformation(
+            model=perceptron_flow, 
+            device=device,
+            train_dataloader=get_cifar10_data(5000)[0],
+            test_dataloader=get_cifar10_data(50000)[1],
+            weight_transformation=clip_and_quantize_model,
+            epochs=cq_quantize_epochs,
+            lr=lr)
+
+    print("Soft core mapping...")
+    soft_core_mapping = SoftCoreMapping()
+    soft_core_mapping.map(perceptron_flow.get_mapper_repr())
+    for idx, core in enumerate(soft_core_mapping.cores):
+        print(f"  Core {idx} matrix shape:", core.core_matrix.shape)
+
+    print("Core flow mapping...")
+    core_flow = CoreFlow(cifar10_input_shape, soft_core_mapping)
+    core_flow.set_activation(CQ_Activation(Tq))
+
+    print("Testing with core flow...")
+    correct, total = test_on_cifar10(core_flow, device)
+    print("  Correct:", correct, "Total:", total)
+
+    print("Hard core mapping...")
+    axons_per_core = 256
+    neurons_per_core = 256
+    hard_core_mapping = HardCoreMapping(axons_per_core, neurons_per_core)
+    hard_core_mapping.map(soft_core_mapping)
+    for idx, core in enumerate(hard_core_mapping.cores):
+        print(f"  Core {idx} matrix shape:", core.core_matrix.shape)
+    
+    print("Quantizing mapped model weights...")
+    clip_core_weights(hard_core_mapping.cores, cifar10_patched_perceptron_test_clipping_rate)
+    scale = ChipQuantization(bits = 4).quantize(hard_core_mapping.cores)
+    print("  Scale:", scale)
+
+    print("Core flow mapping...")
+    core_flow = CoreFlow(cifar10_input_shape, hard_core_mapping)
+    core_flow.set_activation(CQ_Activation(Tq))
+
+    print("Testing with core flow...")
+    correct, total = test_on_cifar10(core_flow, device)
+    print("  Correct:", correct, "Total:", total)
+
+    print("Visualizing final mapping...")
+    HardCoreMappingVisualizer(hard_core_mapping).visualize("../generated/cifar10/final_hard_core_mapping.png")
+    
+    print("Final hard core mapping latency: ", ChipLatency(hard_core_mapping).calculate())
+
+    ######
+    cifar10_input_size = 28*28
+    generated_files_path = "../generated/cifar10/"
+    input_count = 10000
+    _, test_loader = get_cifar10_data(1)
+
+    print("Calculating delay for hard core mapping...")
+    print(f"delay: {ChipLatency(hard_core_mapping).calculate()}")
+
+    print("Mapping hard cores to chip...")
+    chip = hard_cores_to_chip(
+        cifar10_input_size,
+        hard_core_mapping, 
+        axons_per_core,
+        neurons_per_core, 
+        leak=0,
+        weight_type=int)
+
+    print("Saving input data to files...")
+    save_inputs_to_files(generated_files_path, test_loader, input_count)
+
+    print("Saving trained weights and chip generation code...")
+    save_weights_and_chip_code(chip, generated_files_path)
+
+    print("Generating main function code...")
+    simulation_length = int(scale * Tq + ChipLatency(hard_core_mapping).calculate())
+    generate_main_function(
+        generated_files_path, input_count, cifar10_output_size, simulation_length,
+        main_cpp_template, get_config("Deterministic", "Novena", "int"))
+
+    print("Compiling nevresim for mapped chip...")
+    simulator_filename = \
+        compile_simulator(generated_files_path, "../nevresim/")
+    print("Compilation outcome:", simulator_filename)
+
+    print("Executing simulator...")
+    start_time = time.time()
+    chip_output = execute_simulator(simulator_filename, input_count, num_proc=50)
+    end_time = time.time()
+    print("Simulation time:", end_time - start_time)
+
+    print("Evaluating simulator output...")
+    _, test_loader = get_cifar10_data(1)
+    accuracy = evaluate_chip_output(chip_output, test_loader, cifar10_output_size, verbose=True)
+    print("SNN accuracy on CIFAR-10 is:", accuracy*100, "%")
+
+    print("CIFAR-10 perceptron test done.")
