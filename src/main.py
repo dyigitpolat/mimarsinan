290db2df3ec9147be2198bfb0299ac7a8cbaeab8
yigit polat <dyigit96@gmail.com>
2023-03-30 14:56:25 +0800

    main

--- src/main.py
+++ src/main.py
@@ -7,6 +7,9 @@ from mimarsinan.test.cifar10_nni_test.test_cifar10_nni import *
 from mimarsinan.test.cifar100_test.cifar100_test import *
 from mimarsinan.test.cifar100_test.test_cifar100_nni import *
 from mimarsinan.test.mapping_test.test_mapping import *
+from mimarsinan.test.core_flow_test.core_flow_test import *
+from mimarsinan.test.mnist_test.mnist_perceptron_test import *
+from mimarsinan.test.mnist_test.mnist_patched_perceptron_test import *
 
 from mimarsinan.test.debug_spikes_test.debug_spikes_test import *
 
@@ -22,8 +25,11 @@ force_cudnn_initialization()
 # test_mnist()
 # test_mnist_nni()
 # test_mnist_ntk_nni()
-test_cifar10()
+# test_cifar10()
 # test_cifar10_nni()
 # test_cifar100()
 # test_cifar100_nni()
 # test_mapping()
+# test_core_flow()
+# test_mnist_perceptron()
+test_mnist_patched_perceptron()
