d6fc8e153f438c423f9df7d328b325bec39d3742
yigit polat <dyigit96@gmail.com>
2023-04-08 23:33:07 +0800

    add training until target accuracy

--- src/mimarsinan/test/test_utils.py
+++ src/mimarsinan/test/test_utils.py
@@ -117,7 +117,7 @@ def train_with_weight_trasformation(
             optimizer.zero_grad()
             model_a.train()
             model_b.train()
-            nn.CrossEntropyLoss()(model_b(x), y).backward()
+            model_b.loss(x, y).backward()
             transfer_gradients(model_a, model_b)
             optimizer.step()
 
@@ -143,8 +143,105 @@ def train_with_weight_trasformation(
         train_one_epoch(
             model, model_b, optimizer, train_loader, epoch)
 
-        if(epoch % max(epochs // 10, 1) == 0):
+        if(epoch % max(epochs // 10, 4) == 0):
             correct, total = test(model_b, device, test_dataloader)
             print("  Test acc:", correct, '/', total)
 
     update_model_weights(model_b, model)
+
+    correct, total = test(model, device, test_dataloader)
+    print("  Test acc:", correct, '/', total)
+
+    correct, total = test(model, device, train_dataloader)
+    print("  Train acc:", correct, '/', total)
+    return correct / total
+
+
+def train_until_target_accuracy_with_weight_transformation (
+    model, device, 
+    train_dataloader, test_dataloader, 
+    weight_transformation, max_epochs, lr, 
+    target_accuracy):
+
+    # b = a
+    def update_model_weights(from_model, to_model):
+        for from_param, to_param in zip(from_model.parameters(), to_model.parameters()):
+            to_param.data[:] = from_param.data[:]
+
+    def update_model(a, b):
+        update_model_weights(a, b)
+        weight_transformation(b)
+    
+    # d_a = d_b
+    def transfer_gradients(a, b):
+        for a_param, b_param in zip(a.parameters(), b.parameters()):
+            if a_param.requires_grad: 
+                a_param.grad = b_param.grad
+
+    def train_one_epoch(model_a, model_b, optimizer, train_loader, epoch):
+        print("  Training epoch:", epoch)
+        total = 0
+        correct = 0
+        for (x, y) in train_loader:
+            update_model(model_a, model_b)
+            optimizer.zero_grad()
+            model_a.train()
+            model_b.train()
+            model_b.loss(x, y).backward()
+            transfer_gradients(model_a, model_b)
+            optimizer.step()
+            
+            _, predicted = model_b.out.max(1)
+            total += float(y.size(0))
+            correct += float(predicted.eq(y).sum().item())
+        
+        return correct / total
+
+    def test(model, device, test_loader):
+        total = 0
+        correct = 0
+        with torch.no_grad():
+            for (x, y) in test_loader:
+                y.to(device)
+                outputs = model.forward(x)
+                _, predicted = outputs.cpu().max(1)
+                total += float(y.size(0))
+                correct += float(predicted.eq(y).sum().item())
+        return correct, total
+
+    print("  LR:", lr)
+
+    model_b = copy.deepcopy(model)
+
+    train_loader = train_dataloader
+    optimizer = torch.optim.Adam(model.parameters(), lr = lr)
+    epochs = 0
+    acc = 0
+    temp_acc = 0
+    while acc < target_accuracy and epochs < max_epochs:
+        optimizer.param_groups[0]['lr'] *= 0.9
+
+        acc = train_one_epoch(
+            model, model_b, optimizer, train_loader, epochs)
+
+        if(epochs % max(epochs // 10, 4) == 0):
+            correct, total = test(model_b, device, test_dataloader)
+            print("  Test acc:", correct, '/', total)
+        
+        if temp_acc > acc:
+            optimizer.param_groups[0]['lr'] *= 2.0
+            temp_acc = 0
+        else:
+            temp_acc = acc
+
+        print("  LR:", optimizer.param_groups[0]['lr'])
+        epochs += 1
+
+    update_model_weights(model_b, model)
+
+    correct, total = test(model, device, test_dataloader)
+    print("  Test acc:", correct, '/', total)
+
+    correct, total = test(model, device, train_dataloader)
+    print("  Train acc:", correct, '/', total)
+    return optimizer.param_groups[0]['lr']
