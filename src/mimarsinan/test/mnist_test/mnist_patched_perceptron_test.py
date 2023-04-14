1a0adcfd57a9a40b48f5921b00d9a8d03dd912d4
yigit polat <dyigit96@gmail.com>
2023-04-15 02:50:56 +0800

    remove old unused code

--- src/mimarsinan/test/mnist_test/mnist_patched_perceptron_test.py
+++ src/mimarsinan/test/mnist_test/mnist_patched_perceptron_test.py
@@ -65,74 +65,6 @@ def test_mnist_patched_perceptron():
     print("Fusing normalization...")
     perceptron_flow.fuse_normalization()
 
-    # lr *= 0.5
-    # perceptron_flow.set_activation(ClampedReLU())
-    # train_until_target_accuracy_with_weight_transformation(
-    #     model=perceptron_flow,
-    #     device=device,
-    #     train_dataloader=get_mnist_data(batch_size)[0],
-    #     test_dataloader=get_mnist_data(50000)[1],
-    #     weight_transformation=clip_model_weights_and_decay,
-    #     max_epochs=max_epochs,
-    #     lr=lr,
-    #     target_accuracy=prev_acc)
-    
-    # print("Tuning model with soft CQ...")
-    # currentTq = Tq_start
-    # while currentTq >= Tq:
-    #     for i in range(0, 20 + 1):
-    #         alpha = 0.5 * i + 0.1
-    #         # shift = \
-    #         #     (1.0+math.atan(alpha/(4*math.pi))) \
-    #         #     / (2 * Tq)
-    #         # print(f"  Tuning with shift = {shift*Tq}/Tq...")
-    #         # perceptron_flow.set_activation(ClampedShiftReLU(shift))
-    #         # train_until_target_accuracy_with_weight_transformation(
-    #         #     model=perceptron_flow, 
-    #         #     device=device,
-    #         #     train_dataloader=get_mnist_data(batch_size)[0],
-    #         #     test_dataloader=get_mnist_data(50000)[1],
-    #         #     weight_transformation=clip_model_weights_and_decay,
-    #         #     max_epochs=max_epochs,
-    #         #     lr=lr,
-    #         #     target_accuracy=prev_acc)
-    #         print(f"  Tuning with alpha = {alpha}...")
-    #         perceptron_flow.set_activation(CQ_Activation_Soft(currentTq, alpha))
-    #         train_until_target_accuracy_with_weight_transformation(
-    #             model=perceptron_flow, 
-    #             device=device,
-    #             train_dataloader=get_mnist_data(batch_size)[0],
-    #             test_dataloader=get_mnist_data(50000)[1],
-    #             weight_transformation=clip_model_weights_and_decay,
-    #             max_epochs=max_epochs,
-    #             lr=lr,
-    #             target_accuracy=prev_acc)
-    #     currentTq = currentTq // 2
-
-    # print("Tuning model with soft CQ and weight quantization...")
-    # perceptron_flow.set_activation(CQ_Activation_Soft(Tq, alpha))
-    # train_until_target_accuracy_with_weight_transformation(
-    #     model=perceptron_flow, 
-    #     device=device,
-    #     train_dataloader=get_mnist_data(batch_size)[0],
-    #     test_dataloader=get_mnist_data(50000)[1],
-    #     weight_transformation=clip_and_quantize_model,
-    #     max_epochs=max_epochs,
-    #     lr=lr,
-    #     target_accuracy=prev_acc)
-    
-    # print("Tuning model with CQ and weight quantization...")
-    # perceptron_flow.set_activation(CQ_Activation(Tq))
-    # train_until_target_accuracy_with_weight_transformation(
-    #     model=perceptron_flow, 
-    #     device=device,
-    #     train_dataloader=get_mnist_data(batch_size)[0],
-    #     test_dataloader=get_mnist_data(50000)[1],
-    #     weight_transformation=clip_and_quantize_model,
-    #     max_epochs=max_epochs,
-    #     lr=lr,
-    #     target_accuracy=prev_acc)
-
     print("Tuning model with CQ and weight quantization...")
     perceptron_flow.set_activation(CQ_Activation(Tq))
     train_until_target_accuracy_with_weight_transformation(
@@ -174,24 +106,6 @@ def test_mnist_patched_perceptron():
     correct, total = test_on_mnist(core_flow, device)
     print("  Correct:", correct, "Total:", total)
 
-    # print("Tuning hard core mapping...")
-    # lr *= 0.5
-    # core_flow.set_activation(CQ_Activation(Tq))
-    # lr, _ = train_until_target_accuracy_with_weight_transformation(
-    #     model=core_flow,
-    #     device=device,
-    #     train_dataloader=get_mnist_data(batch_size)[0],
-    #     test_dataloader=get_mnist_data(50000)[1],
-    #     weight_transformation=clip_and_quantize_model,
-    #     max_epochs=max_epochs,
-    #     lr_max=lr_max,
-    #     lr=lr,
-    #     target_accuracy=prev_acc)
-
-    # print("Updating hard core parameters...")
-    # core_flow.update_cores()
-    # hard_core_mapping.cores = core_flow.cores
-
     print("Quantizing hard core mapping...")
     scale = ChipQuantization(bits = 4).quantize(hard_core_mapping.cores)
     print("  Scale:", scale)
@@ -221,41 +135,6 @@ def test_mnist_patched_perceptron():
     predictions = simulation_driver.predict_spiking(
         test_loader,
         simulation_steps)
-    
-
-
-
-    # print("Mapping hard cores to chip...")
-    # chip = hard_cores_to_chip(
-    #     mnist_input_size,
-    #     hard_core_mapping,
-    #     axons_per_core,
-    #     neurons_per_core,
-    #     leak=0,
-    #     weight_type=int)
-
-    # print("Saving input data to files...")
-    # save_inputs_to_files(generated_files_path, test_loader, input_count)
-
-    # print("Saving trained weights and chip generation code...")
-    # save_weights_and_chip_code(chip, generated_files_path)
-
-    # print("Generating main function code...")
-    # simulation_length = int(scale * simulation_length + ChipLatency(hard_core_mapping).calculate())
-    # generate_main_function(
-    #     generated_files_path, input_count, mnist_output_size, simulation_length,
-    #     main_cpp_template, get_config("Stochastic", "Novena", "int"))
-
-    # print("Compiling nevresim for mapped chip...")
-    # simulator_filename = \
-    #     compile_simulator(generated_files_path, "../nevresim/")
-    # print("Compilation outcome:", simulator_filename)
-
-    # print("Executing simulator...")
-    # start_time = time.time()
-    # chip_output = execute_simulator(simulator_filename, input_count, num_proc=50)
-    # end_time = time.time()
-    # print("Simulation time:", end_time - start_time)
 
     print("Evaluating simulator output...")
     _, test_loader = get_mnist_data(1)
