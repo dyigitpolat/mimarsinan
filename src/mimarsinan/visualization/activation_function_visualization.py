24690df0d4fe1ab6eaee6fdf1b7a3d4cbe252579
yigit polat <dyigit96@gmail.com>
2023-04-17 02:15:47 +0800

    implement visualizer for activation functions

---
+++ src/mimarsinan/visualization/activation_function_visualization.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+import torch
+
+class ActivationFunctionVisualizer:
+    def __init__(self, activation_function):
+        self.activation_function = activation_function
+        self.x = torch.arange(-0.1, 1.1, 0.001)
+        self.y = torch.tensor([self.activation_function(x) for x in self.x])
+
+    def plot(self, filename):
+        plt.grid()
+        plt.axhline(y=0, color='k')
+        plt.axvline(x=0, color='k')
+        plt.plot(self.x, self.y)
+        plt.savefig(filename)
+        plt.clf()
