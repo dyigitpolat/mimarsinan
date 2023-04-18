64fce825cd23098dc14bcfcb2e6b35290d6581c0
yigit polat <dyigit96@gmail.com>
2023-04-18 20:38:47 +0800

    wider visualization

--- src/mimarsinan/visualization/activation_function_visualization.py
+++ src/mimarsinan/visualization/activation_function_visualization.py
@@ -5,7 +5,7 @@ import torch
 class ActivationFunctionVisualizer:
     def __init__(self, activation_function):
         self.activation_function = activation_function
-        self.x = torch.arange(-0.1, 1.1, 0.001)
+        self.x = torch.arange(-1.0, 1.0, 0.001)
         self.y = torch.tensor([self.activation_function(x) for x in self.x])
 
     def plot(self, filename):
