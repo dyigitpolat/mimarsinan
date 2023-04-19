fb3fc77d9893005182f5ba43cfaa99511a198414
yigit polat <dyigit96@gmail.com>
2023-04-19 21:42:57 +0800

    handle reinitialization

--- src/mimarsinan/common/wandb_utils.py
+++ src/mimarsinan/common/wandb_utils.py
@@ -1,9 +1,15 @@
 import wandb
 
 class WandB_Reporter:
+    initialized = False
     def __init__(self, project_name, experiment_name):
-        wandb.login()
+        if not WandB_Reporter.initialized:
+            wandb.login()
+            WandB_Reporter.initialized = True
+        else:
+            wandb.finish()
+            
         wandb.init(project=project_name, name=experiment_name)
-    
+
     def report(self, metric_name, metric_value, step = None):
         wandb.log({metric_name: metric_value}, step = step)
