5e9b0003ba9dd9f76c07720e828ba1b2e37ca448
yigit polat <dyigit96@gmail.com>
2023-04-19 22:23:58 +0800

    tweak smart smooth adaptation

--- src/mimarsinan/tuning/smart_smooth_adaptation.py
+++ src/mimarsinan/tuning/smart_smooth_adaptation.py
@@ -10,28 +10,36 @@ class SmartSmoothAdaptation(BasicSmoothAdaptation):
         self.state_clone_function = state_clone_function
         self.state_restore_function = state_restore_function
         self.evaluation_function = evaluation_function
-        self.tolerance = 0.1
+        self.tolerance = 0.01
+        self.step_epsilon = 0.0001
 
     def _find_step_size(self, t, interpolators):
         step_size = (1 - t)
         state = self.state_clone_function()
         prev_metric = self.evaluation_function(*[i(t) for i in interpolators])
 
-        while current_metric < prev_metric * (1 - self.tolerance):
+        current_metric = 0
+        lower_bound = prev_metric * (1 - self.tolerance)
+        while current_metric < lower_bound and step_size > self.step_epsilon:
+            print("step_size: ", step_size)
             next_t = t + step_size
             current_metric = self.evaluation_function(*[i(next_t) for i in interpolators])
 
-            if current_metric < prev_metric * (1 - self.tolerance):
+            if current_metric < lower_bound:
                 self.state_restore_function(state)
                 step_size /= 2
 
+        print("current_metric: ", current_metric)
+        print("prev_metric: ", prev_metric)
         return step_size
 
     def adapt_smoothly(self, interpolators, max_cycles):
         t = 0
         cycles = 0
+        self.adaptation_function(*[i(t) for i in interpolators])
         while t < 1 and cycles < max_cycles:
-            step_size = self._find_step_size()
+            step_size = self._find_step_size(t, interpolators)
             t += step_size
             self.adaptation_function(*[i(t) for i in interpolators])
+            cycles += 1
 
