c9b3bb5926573c15118a45d1fffbfb9b2cb049a6
yigit polat <dyigit96@gmail.com>
2023-04-19 21:44:05 +0800

    remove deprecated training functions cifar 10

--- src/mimarsinan/test/cifar10_test/cifar10_test_utils.py
+++ src/mimarsinan/test/cifar10_test/cifar10_test_utils.py
@@ -41,76 +41,10 @@ def get_cifar10_data(batch_size=1):
     test_loader = torch.utils.data.DataLoader(
         test_set, batch_size=batch_size, shuffle=False, num_workers=32,
         pin_memory=True)
+    validation_loader = torch.utils.data.DataLoader(
+        test_set, batch_size=batch_size, shuffle=True, num_workers=0)
 
-    return train_loader, test_loader
-
-def train_on_cifar10_for_one_epoch(ann, device, optimizer, train_loader, epoch):
-    print("Training epoch:", epoch)
-    for (x, y) in train_loader:
-        optimizer.zero_grad()
-        ann.train()
-        y.to(device)
-        outputs = ann.forward(x)
-        loss = nn.CrossEntropyLoss()(outputs, y)
-        loss.backward()
-        optimizer.step()
-
-def train_on_cifar10(ann, device, epochs, lr = 0.001, batch_size = 5000):
-    train_loader, _ = get_cifar10_data(batch_size)
-    optimizer = torch.optim.Adam(ann.parameters(), lr = lr, weight_decay=0.00005)
-    
-    for epoch in range(epochs):
-        train_on_cifar10_for_one_epoch(
-            ann, device, optimizer, train_loader, epoch)
-
-        if(epoch % max(epochs // 10, 1) == 0):
-            correct, total = test_on_cifar10(ann, device)
-            print(correct, '/', total)
-
-def test_on_cifar10(ann, device):
-    total = 0
-    correct = 0
-    with torch.no_grad():
-        _, test_loader = get_cifar10_data(10000)
-        for (x, y) in test_loader:
-            y.to(device)
-            outputs = ann.forward(x)
-            _, predicted = outputs.cpu().max(1)
-            total += float(y.size(0))
-            correct += float(predicted.eq(y).sum().item())
-    
-    return correct, total
-
-def get_mlp_mixer_model(parameters):
-    cifar10_h = 32
-    cifar10_w = 32
-    cifar10_c = 3
-    cifar10_output_size = 10
-
-    region_borders_x = get_region_borders(
-        int(parameters['patch_cols']), 
-        int(parameters['patch_center_x']), 
-        int(parameters['patch_lensing_exp_x']),
-        cifar10_w)
-
-    region_borders_y = get_region_borders(
-        int(parameters['patch_rows']), 
-        int(parameters['patch_center_y']), 
-        int(parameters['patch_lensing_exp_y']),
-        cifar10_h)
-        
-    return SimpleMLPMixer(
-        int(parameters['patch_rows']), int(parameters['patch_cols']),
-        int(parameters['features_per_patch']),
-        int(parameters['mixer_channels']),
-        int(parameters['mixer_features']),
-        int(parameters['inner_mlp_width']),
-        int(parameters['inner_mlp_count']),
-        region_borders_x,
-        region_borders_y,
-        cifar10_h,cifar10_w,cifar10_c, 
-        cifar10_output_size)
-
+    return train_loader, test_loader, validation_loader
 
 
 
