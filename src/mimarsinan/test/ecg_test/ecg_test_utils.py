f3ccdacc986c6261835ecd3ca29c292dcdda4550
yigit polat <dyigit96@gmail.com>
2023-04-28 18:39:18 +0800

    simplify ecg test utils and finalize ecg test flow

--- src/mimarsinan/test/ecg_test/ecg_test_utils.py
+++ src/mimarsinan/test/ecg_test/ecg_test_utils.py
@@ -1,69 +1,16 @@
-import wfdb
 import numpy as np
 from torch.utils.data import Dataset
 from torchvision.transforms import ToTensor
 import torch
 
-import os
-
-path = '/home/yigit/data/ecg_data_normalize_smoke_2c.npz'  
-f = np.load(path)
-train_x, train_y = f['x_train'], f['y_train']
-test_x, test_y = f['x_test'], f['y_test']
-
-y_test_ = test_y
-X_train_ = torch.FloatTensor(train_x)
-print(X_train_.shape)
-
-X_train_ = X_train_.reshape(-1,1,180,1)
-y_train_ = torch.FloatTensor(train_y)
-    
-X_test = torch.FloatTensor(test_x)
-X_test = X_test.reshape(-1,1,180,1)
-print(X_test.shape)
-
-y_test = torch.FloatTensor(test_y)
-
 #define a transform that shifts x within the range of 10% of its length
 def shift(x):
-    shift = int(0.5 * x.shape[1])
-    shift = np.random.randint(-shift, shift)
+    shift_amt = np.random.uniform(-0.3, 0.3)
+    shift = int(shift_amt * x.shape[1])
 
-    x = x * np.random.uniform(0.5, 2.5)
-    random_mask = torch.rand(x.shape) > 0.5
-    x[random_mask] = 0
-    random_mask = torch.rand(x.shape) > 0.2
-    x[random_mask] *= 2
-
+    x = x * np.random.uniform(0.5, 1.5)
+    x = torch.clamp(x, min=0, max=1)
     return torch.roll(x, shift, dims=1)
-
-torch_dataset_train = torch.utils.data.TensorDataset(X_train_, y_train_)
-torch_dataset_test = torch.utils.data.TensorDataset(X_test, y_test)
-
-class CustomTensorDataset(Dataset):
-    def __init__(self, data, targets, transform=None):
-        assert data.size(0) == targets.size(0)  # Ensure data and targets have the same length
-        self.data = data
-        self.targets = targets
-        self.transform = transform
-
-    def __getitem__(self, index):
-        x = self.data[index]
-        y = self.targets[index]
-
-        if self.transform:
-            x = self.transform(x)
-
-        return x, y
-
-    def __len__(self):
-        return self.data.size(0)
-    
-custom_train_set = CustomTensorDataset(X_train_, y_train_, transform=shift)
-
-train_loader = torch.utils.data.DataLoader(custom_train_set,shuffle=True,batch_size=4296, num_workers=16, pin_memory=True)
-test_loader = torch.utils.data.DataLoader(torch_dataset_test, shuffle=False,batch_size=49691, num_workers=16, pin_memory=True)
-
 
 
 def plot_array(array, title=None):
@@ -102,12 +49,79 @@ def normalize_array(array):
     # return torch.where(array > 0, array ** 0.9, -((-array) ** 0.9))
     return 0.6*torch.tanh(0.5*torch.pi*array) / (0.5*torch.pi) + 0.4*array
 
+
+def get_ecg_data(batch_size):
+    path = '/home/yigit/data/ecg_data_normalize_smoke_2c.npz'  
+    f = np.load(path)
+    train_x, train_y = f['x_train'], f['y_train']
+    test_x, test_y = f['x_test'], f['y_test']
+
+    X_train_ = torch.FloatTensor(train_x)
+    X_train_ = X_train_.reshape(-1,1,180,1)
+    print(X_train_.shape)
+
+    y_train_ = torch.LongTensor(train_y)
+        
+    X_test = torch.FloatTensor(test_x)
+    X_test = X_test.reshape(-1,1,180,1)
+    print(X_test.shape)
+
+    y_test_ = test_y
+    y_test = torch.LongTensor(test_y)
+
+    class CustomTensorDataset(Dataset):
+        def __init__(self, data, targets, transform=None):
+            assert data.size(0) == targets.size(0)  # Ensure data and targets have the same length
+            self.data = data
+            self.targets = targets
+            self.transform = transform
+
+        def __getitem__(self, index):
+            x = self.data[index]
+            y = self.targets[index]
+
+            if self.transform:
+                x = self.transform(x)
+
+            return x, y
+
+        def __len__(self):
+            return self.data.size(0)
+        
+    train_pairs = [(X_train_[i], y_train_[i]) for i in range(len(X_train_))]
+    # oversample minority classes
+    train_pairs_0 = [pair for pair in train_pairs if pair[1] == 0]
+    train_pairs_1 = [pair for pair in train_pairs if pair[1] == 1]
+    train_pairs_1_over = train_pairs_1 * int(len(train_pairs_0)/len(train_pairs_1))
+    train_pairs_over = train_pairs_0 + train_pairs_1_over
+    np.random.shuffle(train_pairs_over)
+    X_train_o = torch.stack([pair[0] for pair in train_pairs_over])
+    y_train_o = torch.stack([pair[1] for pair in train_pairs_over])
+
+    custom_train_set = CustomTensorDataset(X_train_o, y_train_o, transform=shift)
+
+    torch_dataset_train = torch.utils.data.TensorDataset(X_train_o, y_train_o)
+    torch_dataset_test = torch.utils.data.TensorDataset(X_test, y_test)
+    torch_dataset_validation = torch.utils.data.TensorDataset(X_train_, y_train_)
+
+    train_loader = torch.utils.data.DataLoader(custom_train_set,shuffle=True,batch_size=batch_size, num_workers=4, pin_memory=True)
+    test_loader = torch.utils.data.DataLoader(torch_dataset_test, shuffle=False,batch_size=batch_size, num_workers=4, pin_memory=True)
+    validation_loader = torch.utils.data.DataLoader(torch_dataset_validation, shuffle=True,batch_size=batch_size, num_workers=4, pin_memory=True)
+
+    return train_loader, test_loader, validation_loader
+
 # Example usage
 if __name__ == "__main__":
+    device = 'cuda'
+
+    train_loader = get_ecg_data(4296)[0]
+    test_loader = get_ecg_data(49691)[1]
+
     for x, y in train_loader:
         print(x.shape)
         print(y.shape)
         plot_2d_img_array(x.squeeze().numpy().T, y.numpy(), title="Raw ECG")
+        plot_array(x.squeeze().numpy()[0], title="Raw ECG dp 0")
 
         break
 
@@ -115,24 +129,18 @@ if __name__ == "__main__":
 
     net = nn.Sequential(
 
-        nn.Linear(180, 4),
+        nn.Linear(180, 128),
         nn.LeakyReLU(),
 
-        nn.Linear(4, 4),
-        nn.LeakyReLU(),
-        nn.Dropout(0.5),
-
-
-        nn.Linear(4, 4),
+        nn.Linear(128, 128),
         nn.LeakyReLU(),
         nn.Dropout(0.5),
 
-
-        nn.Linear(4, 2),
+        nn.Linear(128, 64),
         nn.LeakyReLU(),
         nn.Dropout(0.5),
 
-        nn.Linear(2, 2),
+        nn.Linear(64, 2),
         nn.LeakyReLU(),
 
         nn.Softmax(dim=1)
@@ -143,23 +151,34 @@ if __name__ == "__main__":
 
     def test():
         with torch.no_grad():
-            correct = 0
-            total = 0
+            true_positive = 0
+            true_negative = 0
+            total_positive = 0
+            total_negative = 0
             for x, y in test_loader:
-                x, y = x.to('cuda'), y.to('cuda')
+                x, y = x.to(device), y.to(device)
                 out = net(x.squeeze())
                 _, predicted = torch.max(out.data, 1)
-                total += y.size(0)
-                correct += (predicted == y).sum().item()
+                total_positive += (y == 1).sum().item()
+                total_negative += (y == 0).sum().item()
+                true_positive += ((predicted == 1) & (y == 1)).sum().item()
+                true_negative += ((predicted == 0) & (y == 0)).sum().item()
 
-            print("Accuracy: {}".format(correct / total))
+            # print confusion matrix as np grid
+            print("Confusion matrix:")
+            print(np.array([[true_negative, total_negative - true_negative],
+                            [total_positive - true_positive, true_positive]]))
+            
+            print("Test Accuracy: {}".format((true_positive + true_negative) / (total_positive + total_negative)))
+            
+
 
     def test_on_trainset():
         with torch.no_grad():
             correct = 0
             total = 0
             for x, y in train_loader:
-                x, y = x.to('cuda'), y.to('cuda')
+                x, y = x.to(device), y.to(device)
                 out = net(x.squeeze())
                 _, predicted = torch.max(out.data, 1)
                 total += y.size(0)
@@ -169,11 +188,11 @@ if __name__ == "__main__":
 
     import time
 
-    net.to('cuda')
-    for epoch in range(100):
+    net.to(device)
+    for epoch in range(200):
         start_time = time.time()
         for x, y in train_loader:
-            x, y = x.to('cuda'), y.to('cuda')
+            x, y = x.to(device), y.to(device)
 
             optimizer.zero_grad()
             out = net(x.squeeze())
