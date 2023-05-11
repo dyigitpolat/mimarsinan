d0c14f63157c48cb111fac5523b412110859862e
yigit polat <dyigit96@gmail.com>
2023-05-11 20:26:41 +0800

    parameter transforms collection

---
+++ src/mimarsinan/transformations/parameter_transforms/collection.py
@@ -0,0 +1,44 @@
+from mimarsinan.transformations.weight_quantization import TensorQuantization
+from mimarsinan.transformations.weight_clipping import SoftTensorClipping
+
+import torch
+
+pipeline_clipping_rate = 0.01
+
+def special_decay(w):
+    return torch.clamp(w, -1, 1)
+
+def decay_param(param_data):
+    out = special_decay(param_data)
+    return out
+
+def decay_and_quantize_param(param_data):
+    quantizer = TensorQuantization(bits=4)
+
+    out = special_decay(param_data)
+    out = quantizer.quantize(out)
+    return out
+
+def clip_decay_and_quantize_param(param_data):
+    clipper = SoftTensorClipping(pipeline_clipping_rate)
+    quantizer = TensorQuantization(bits=4)
+
+    out = clipper.get_clipped_weights(param_data)
+    out = special_decay(out)
+    out = quantizer.quantize(out)
+    return out
+
+def clip_and_decay_param(param_data):
+    clipper = SoftTensorClipping(pipeline_clipping_rate)
+
+    out = clipper.get_clipped_weights(param_data)
+    out = special_decay(out)
+    return out
+
+def clip_and_quantize_param(param_data):
+    clipper = SoftTensorClipping(pipeline_clipping_rate)
+    quantizer = TensorQuantization(bits=4)
+
+    out = clipper.get_clipped_weights(param_data)
+    out = quantizer.quantize(out)
+    return out
