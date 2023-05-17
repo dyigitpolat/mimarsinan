ab900eafc0988716652454b0d6321643b9f49ea0
yigit polat <dyigit96@gmail.com>
2023-05-18 05:36:38 +0800

    improve wq tuner

--- src/mimarsinan/pipelining/weight_quantization_tuner.py
+++ src/mimarsinan/pipelining/weight_quantization_tuner.py
@@ -2,35 +2,80 @@ from mimarsinan.model_training.weight_transform_trainer import WeightTransformTr
 from mimarsinan.transformations.parameter_transforms.collection import *
 from mimarsinan.tuning.learning_rate_explorer import LearningRateExplorer
 from mimarsinan.models.layers import CQ_Activation
+from mimarsinan.tuning.basic_interpolation import BasicInterpolation
+from mimarsinan.tuning.basic_smooth_adaptation import BasicSmoothAdaptation
 
 class WeightQuantizationTuner:
     def __init__(self, pipeline, epochs, target_tq, target_accuracy):
+        # Targets
+        self.target_tq = target_tq
+        self.target_accuracy = target_accuracy
+
+        # Model
+        self.model = pipeline.model
+        self.model.set_activation(CQ_Activation(self.target_tq))
+
+        def mixed_transform(rate):
+            def transform(param):
+                a = clip_decay_and_quantize_param(param)
+                b = param
+                return a * rate + b * (1.0 - rate)
+            return transform
+
         # Trainer
         self.trainer = WeightTransformTrainer(
             self.model, 
             pipeline.device, 
             pipeline.training_dataloader, 
             pipeline.validation_dataloader, 
-            pipeline.wq_loss, clip_decay_and_quantize_param)
-        self.trainer.report_function = pipeline.reporter.report
+            pipeline.wq_loss, mixed_transform(0.5))
+        
+        def report(key, value):
+            if key == "Training accuracy":
+                print(f"WQ accuracy: {value}")
+            pipeline.reporter.report(key, value)
+
+        self.trainer.report_function = report
 
         # Automatic learning rate
-        desired_improvement = -(1.0 / self.num_classes) / 100
-        self.tuned_lr = LearningRateExplorer(
+        self.lr = LearningRateExplorer(
             self.trainer, 
             self.model, 
-            max_lr=1e-1, 
-            min_lr=1e-5, 
-            desired_improvement=desired_improvement).find_lr_for_tuning()
+            pipeline.lr, 
+            pipeline.lr / 1000, 
+            0.1 / pipeline.num_classes).find_lr_for_tuning()
         
         # Epochs
         self.epochs = epochs
+        self.cycles = 10
 
         # Targets
         self.target_tq = target_tq
         self.target_accuracy = target_accuracy
 
+        # Adaptation
+        self._prev_acc = target_accuracy
+        def adaptation(q_rate):
+            print(f"  Tuning model with q_rate = {q_rate}...")
+            pipeline.reporter.report("Quantization Rate", q_rate)
+
+            self.model.set_activation(CQ_Activation(self.target_tq))
+            self.trainer.weight_transformation = mixed_transform(q_rate)
+
+            acc = self.trainer.train_until_target_accuracy(
+                self.lr, self.epochs, self._prev_acc)
+            
+            self._prev_acc = max(self._prev_acc, acc)
+            
+        self.adaptation_function = adaptation
+
     def run(self):
         self.model.set_activation(CQ_Activation(self.target_tq))
-        self.trainer.train_until_target_accuracy(
-            self.tuned_lr, self.epochs, self.target_accuracy)
+
+        BasicSmoothAdaptation (
+            self.adaptation_function
+        ).adapt_smoothly(
+            interpolators=[BasicInterpolation(0.0, 1.0)], 
+            cycles=self.cycles)
+        
+        return self.trainer.validate_train()
