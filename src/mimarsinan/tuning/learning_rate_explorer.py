f8757f9498523bb5ac7188fd7aab56107f701b32
yigit polat <dyigit96@gmail.com>
2023-05-18 05:37:20 +0800

    optional base accuracy parameter for lr explorer

--- src/mimarsinan/tuning/learning_rate_explorer.py
+++ src/mimarsinan/tuning/learning_rate_explorer.py
@@ -1,23 +1,30 @@
 class LearningRateExplorer:
-    def __init__(self, trainer, model, max_lr, min_lr, desired_improvement = 0.0):
+    def __init__(self, 
+                 trainer, model, max_lr, min_lr, 
+                 desired_improvement = 0.0, base_accuracy = None):
         self.training_function = trainer.train_one_step
         self.evaluation_function = trainer.validate_train
         self.model = model
         self.max_lr = max_lr
         self.min_lr = min_lr
         self.desired_improvement = desired_improvement
+        self.base_accuracy = base_accuracy
 
     def find_lr_for_tuning(self):
         original_state = self.model.state_dict()
-        original_acc = self.evaluation_function()
-        target_acc = original_acc * ( 1.0 + self.desired_improvement )
+
+        if self.base_accuracy is None:
+            self.base_accuracy = self.evaluation_function()
+
+        target_acc = self.base_accuracy * ( 1.0 + self.desired_improvement )
 
         lr = self.max_lr
         acc = 0
         while acc < target_acc and lr > self.min_lr:
+            print(f"  Trying lr = {lr}")
             self.training_function(lr)
             acc = self.evaluation_function()
-            if acc < original_acc:
+            if acc < target_acc:
                 lr *= 0.9
             
             self.model.load_state_dict(original_state)
