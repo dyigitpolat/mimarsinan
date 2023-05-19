027acbe9e7591bbaf3da191887a89fd5ec32aa70
yigit polat <dyigit96@gmail.com>
2023-05-19 21:07:26 +0800

    refine smart smooth adaptation

--- src/mimarsinan/tuning/smart_smooth_adaptation.py
+++ src/mimarsinan/tuning/smart_smooth_adaptation.py
@@ -11,7 +11,11 @@ class SmartSmoothAdaptation(BasicSmoothAdaptation):
         self.state_restore_function = state_restore_function
         self.evaluation_function = evaluation_function
         self.tolerance = 0.01
-        self.step_epsilon = 0.0001
+        self.min_step = 0.0001
+
+    def _adjust_minimum_step(self, step_size, t):
+        if step_size < self.min_step:
+            self.min_step *= 2.0
 
     def _lower_bound(self, t, interpolators):
         prev_metric = self.evaluation_function(*[i(t) for i in interpolators])
@@ -23,7 +27,7 @@ class SmartSmoothAdaptation(BasicSmoothAdaptation):
 
         current_metric = 0
         lower_bound = self._lower_bound(t, interpolators)
-        while current_metric < lower_bound and step_size > self.step_epsilon:
+        while current_metric < lower_bound and step_size > self.min_step:
             print("step_size: ", step_size)
             next_t = t + step_size
             current_metric = self.evaluation_function(*[i(next_t) for i in interpolators])
@@ -32,14 +36,15 @@ class SmartSmoothAdaptation(BasicSmoothAdaptation):
                 self.state_restore_function(state)
                 step_size /= 2
 
+        self._adjust_minimum_step(step_size, t)
+
         print("current_metric: ", current_metric)
         return step_size
 
-    def adapt_smoothly(self, interpolators, max_cycles):
+    def adapt_smoothly(self, interpolators, max_cycles = None):
         t = 0
         cycles = 0
-        self.adaptation_function(*[i(t) for i in interpolators])
-        while t < 1 and cycles < max_cycles:
+        while t < 1 and (not max_cycles or cycles < max_cycles):
             step_size = self._find_step_size(t, interpolators)
             t += step_size
             self.adaptation_function(*[i(t) for i in interpolators])
