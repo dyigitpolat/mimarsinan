2b686654bfec5e29379e4e4472a13104642b425b
yigit polat <dyigit96@gmail.com>
2023-05-19 21:00:41 +0800

    visualisation in hard core mapping

--- src/mimarsinan/pipelining/hard_core_mapper.py
+++ src/mimarsinan/pipelining/hard_core_mapper.py
@@ -1,5 +1,6 @@
 from mimarsinan.transformations.chip_quantization import ChipQuantization
 from mimarsinan.mapping.softcore_mapping import HardCoreMapping
+from mimarsinan.visualization.hardcore_visualization import HardCoreMappingVisualizer
 
 class HardCoreMapper:
     def __init__(self, pipeline, soft_core_mapping):
@@ -7,6 +8,7 @@ class HardCoreMapper:
         self.soft_core_mapping = soft_core_mapping
         self.axons_per_core = pipeline.max_axons
         self.neurons_per_core = pipeline.max_neurons
+        self.wd = pipeline.working_directory
 
     def run(self):
         ChipQuantization(bits = 4).calculate_core_thresholds(
@@ -16,6 +18,10 @@ class HardCoreMapper:
             self.axons_per_core, self.neurons_per_core)
         
         hard_core_mapping.map(self.soft_core_mapping)
+
+        HardCoreMappingVisualizer(hard_core_mapping).visualize(
+            self.wd + "/hardcore_mapping.png"
+        )
 
         return hard_core_mapping
     
