6fb0a92ea17608105c049706af13f48e78220c1c
yigit polat <dyigit96@gmail.com>
2023-05-19 21:01:07 +0800

    emit accuracy after norm fusion

--- src/mimarsinan/pipelining/normalization_fuser.py
+++ src/mimarsinan/pipelining/normalization_fuser.py
@@ -1,6 +1,19 @@
+from mimarsinan.model_training.basic_trainer import BasicTrainer
+
 class NormalizationFuser:
     def __init__(self, pipeline):
+        # Model
         self.model = pipeline.model
+
+        # Trainer
+        self.trainer = BasicTrainer(
+            self.model, 
+            pipeline.device, 
+            pipeline.training_dataloader, 
+            pipeline.validation_dataloader, 
+            pipeline.wq_loss)
 
     def run(self):
         self.model.fuse_normalization()
+
+        return self.trainer.validate_train()
