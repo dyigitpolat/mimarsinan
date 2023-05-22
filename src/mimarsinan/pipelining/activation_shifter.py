14cf831286a6212bfd0ecd006ae9ae3f7a13726c
yigit polat <dyigit96@gmail.com>
2023-05-23 00:49:19 +0800

    improve AS

--- src/mimarsinan/pipelining/activation_shifter.py
+++ src/mimarsinan/pipelining/activation_shifter.py
@@ -1,26 +1,24 @@
-from mimarsinan.model_training.weight_transform_trainer import WeightTransformTrainer
-from mimarsinan.tuning.learning_rate_explorer import LearningRateExplorer
+from mimarsinan.model_training.basic_trainer import BasicTrainer
 from mimarsinan.transformations.parameter_transforms.collection import *
 from mimarsinan.models.layers import ShiftedActivation, ClampedReLU
-from mimarsinan.tuning.basic_interpolation import BasicInterpolation
-from mimarsinan.tuning.smart_smooth_adaptation import SmartSmoothAdaptation
 
 import torch.nn as nn
 class ActivationShifter:
     def __init__(self, pipeline, epochs, target_tq, target_accuracy):
         # Targets
         self.target_tq = target_tq
+        self.target_accuracy = target_accuracy * 0.99
 
         # Model
         self.model = pipeline.model
 
         # Trainer
-        self.trainer = WeightTransformTrainer(
+        self.trainer = BasicTrainer(
             self.model, 
             pipeline.device, 
             pipeline.training_dataloader, 
             pipeline.validation_dataloader, 
-            pipeline.aq_loss, clip_and_decay_param)
+            pipeline.aq_loss)
         
         def report(key, value):
             if key == "Training accuracy":
@@ -47,9 +45,7 @@ class ActivationShifter:
 
         
     def run(self):
-        for i in range(1, self.cycles + 1):
-            print(f"  Activation shift: 0.5 * ({i}/{self.cycles}) / {self.target_tq}")
-            shift_amount = 0.5 * (i / self.cycles) / self.target_tq
-            self.shift_activation(shift_amount)
-            self.trainer.train_n_epochs(self.lr / 20, 2)
+        shift_amount = 0.5 / self.target_tq
+        self.shift_activation(shift_amount)
+        self.trainer.train_until_target_accuracy(self.lr / 20, 2, self.target_accuracy)
         return self.trainer.validate_train()
