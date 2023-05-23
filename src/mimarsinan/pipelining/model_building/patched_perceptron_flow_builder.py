5e99268afd181f343d8151ad48db60d97a9b3c6e
yigit polat <dyigit96@gmail.com>
2023-05-23 21:24:46 +0800

    find the largest patch size

--- src/mimarsinan/pipelining/model_building/patched_perceptron_flow_builder.py
+++ src/mimarsinan/pipelining/model_building/patched_perceptron_flow_builder.py
@@ -1,5 +1,6 @@
 from mimarsinan.models.perceptron_flow import PatchedPerceptronFlow
 
+import math
 class PatchedPerceptronFlowBuilder:
     def __init__(self, 
                  max_axons, max_neurons,
@@ -10,13 +11,24 @@ class PatchedPerceptronFlowBuilder:
         self.output_shape = output_shape
 
     def build(self):
-        patch_rows = max(self.input_shape[-2] // 4, 1)
-        patch_cols = max(self.input_shape[-1] // 4, 1)
+        patch_cols = -1
+        patch_rows = -1
+        for i in range(1, int(math.sqrt(self.max_axons)) + 1):
+            try:
+                patch_rows = max(self.input_shape[-2] // i, 1)
+                patch_cols = max(self.input_shape[-1] // i, 1)
 
-        perceptron_flow = PatchedPerceptronFlow(
-            self.input_shape, self.output_shape,
-            self.max_axons - 1, self.max_neurons,
-            patch_rows, 
-            patch_cols, fc_depth=2)
+                perceptron_flow = PatchedPerceptronFlow(
+                    self.input_shape, self.output_shape,
+                    self.max_axons - 1, self.max_neurons,
+                    patch_rows, 
+                    patch_cols, fc_depth=2)
+                break
+            except: 
+                continue
+        
+        print(f"Patch dimensions {patch_cols} x {patch_cols}")
+        assert patch_cols != -1 or patch_rows != -1, "Could not find a valid patch size for the given input shape and max axons."
+
     
         return perceptron_flow
