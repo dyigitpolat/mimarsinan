633aabcfe811264804092c2634351e21e02bd5f0
yigit polat <dyigit96@gmail.com>
2023-05-30 15:54:59 +0800

    refactor mnist data provider

--- src/mimarsinan/data_handling/data_providers/mnist_data_provider.py
+++ src/mimarsinan/data_handling/data_providers/mnist_data_provider.py
@@ -1,36 +1,39 @@
 from mimarsinan.data_handling.data_provider import DataProvider
 
-
 import torchvision.transforms as transforms
 import torchvision
+
+import torch
 
 class MNIST_DataProvider(DataProvider):
     datasets_path = "../datasets"
 
     def __init__(self):
         super().__init__()
-        self.datasets = {}
 
-    def _get_training_dataset(self):
-        if "training" not in self.datasets:
-            self.datasets["training"] = torchvision.datasets.MNIST(
+        base_training_dataset = torchvision.datasets.MNIST(
             root=self.datasets_path, train=True, download=True,
             transform=transforms.ToTensor())
 
-        return self.datasets["training"]
+        training_validation_split = 0.99
+        
+        base_training_length = len(base_training_dataset)
+        training_length = int(base_training_length * training_validation_split)
+        validation_length = base_training_length - training_length
 
-    def _get_validation_dataset(self):
-        if "validation" not in self.datasets:
-            self.datasets["validation"] = torchvision.datasets.MNIST(
-            root=self.datasets_path, train=True, download=True,
-            transform=transforms.ToTensor())
+        self.training_dataset, self.validation_dataset = \
+            torch.utils.data.random_split( 
+                base_training_dataset, (training_length, validation_length))
 
-        return self.datasets["validation"]
-
-    def _get_test_dataset(self):
-        if "test" not in self.datasets:
-            self.datasets["test"] = torchvision.datasets.MNIST(
+        self.test_dataset = torchvision.datasets.MNIST(
             root=self.datasets_path, train=False, download=True,
             transform=transforms.ToTensor())
 
-        return self.datasets["test"]
+    def _get_training_dataset(self):
+        return self.training_dataset
+
+    def _get_validation_dataset(self):
+        return self.validation_dataset
+
+    def _get_test_dataset(self):
+        return self.test_dataset
