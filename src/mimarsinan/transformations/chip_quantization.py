71f7aea4c3a2acc02ba6c46b52b1409fb95f4a4b
yigit polat <dyigit96@gmail.com>
2023-06-08 21:11:58 +0800

    refactor quantization

--- src/mimarsinan/transformations/chip_quantization.py
+++ src/mimarsinan/transformations/chip_quantization.py
@@ -6,34 +6,14 @@ class ChipQuantization:
     def __init__(self, bits):
         self.quantizer = TensorQuantization(bits)
         self.target_minimum_threshold = self.quantizer.q_max
-
-    def calculate_threshold(self, weights):
-        max_w = np.max(weights)
-        min_w = np.min(weights)
-
-        return self.quantizer.get_scale(min_w, max_w)
-    
-    def scale_thresholds(self, cores):
-        min_threshold = min([core.threshold for core in cores])
-        target = self.target_minimum_threshold
-
-        if(min_threshold < target):
-            scale = target / min_threshold
-        else:
-            scale = 1.0
-
-        for core in cores:
-            core.threshold *= scale
-
-        return scale
     
     def calculate_core_thresholds(self, cores):
         for core in cores:
-            core.threshold = self.quantizer.get_scale(core.core_matrix)
+            core.threshold = self.quantizer.q_max
     
     def quantize(self, cores):
         self.calculate_core_thresholds(cores)
-        scale = self.scale_thresholds(cores)
+        scale = 1.0
             
         for core in cores:
             core.threshold = round(core.threshold)
