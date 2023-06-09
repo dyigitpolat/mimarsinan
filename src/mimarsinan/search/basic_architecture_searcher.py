cb5333a4c3188015fe385dccf18233e6fa866669
yigit polat <dyigit96@gmail.com>
2023-06-10 00:12:29 +0800

    architecture srch and configuration sampling algo

---
+++ src/mimarsinan/search/basic_architecture_searcher.py
@@ -0,0 +1,35 @@
+class BasicArchitectureSearcher:
+    def __init__(self):
+        pass
+
+    def _evaluate_architecture(self, configuration):
+        raise NotImplementedError
+
+    def _create_model(self, configuration):
+        raise NotImplementedError
+
+    def _sample_configurations(self, n):
+        raise NotImplementedError
+
+    def _validate_configuration(self, configuration):
+        raise NotImplementedError
+    
+    def _update_sampler(self, metrics):
+        raise NotImplementedError
+
+    def get_optimized_model(self):
+        cycles = 10
+        configuration_batch_size = 100
+        for _ in range(cycles):
+            configurations = self._sample_configurations(configuration_batch_size)
+
+            metrics = []
+            for configuration in configurations:
+                if self._validate_configuration(configuration):
+                    metrics.append(
+                        (configuration, self._evaluate_architecture(configuration)))
+            
+            self._update_sampler(metrics)
+            best_configuration = max(metrics, key = lambda x: x[1])
+        
+        return self._create_model(best_configuration)
---
+++ src/mimarsinan/search/basic_configuration_sampler.py
@@ -0,0 +1,47 @@
+import random 
+
+class BasicConfigurationSampler:
+    def __init__(self, configuration_space):
+        self.previous_metrics = None
+        self.space = configuration_space
+        self.refined_space = None
+
+    def _sample_from_space(self, space):
+        sample = []
+        for key in space:
+            sample.append(random.choice(space[key]))
+
+        return sample
+
+    def update(self, metrics):
+        if self.previous_metrics is not None:
+            self.previous_metrics += metrics
+            self.previous_metrics.sort(key = lambda x: x[1])
+            length = len(self.previous_metrics)
+            self.previous_metrics = self.previous_metrics[:length//2]
+        else:
+            self.previous_metrics = metrics
+
+        top_p = 0.1
+        top_metric_count = int(len(self.previous_metrics) * top_p)
+        top_metrics = self.previous_metrics[:top_metric_count]
+
+        self.refined_space = {}
+        for metric in top_metrics:
+            configuration = metric[0]
+            for key in configuration:
+                if key not in self.refined_space:
+                    self.refined_space[key] = []
+                self.refined_space[key].append(configuration[key])
+
+    def sample(self, n):
+        samples = []
+        refined_samples_p = 0.5
+        refined_samples_count = int(n * refined_samples_p)
+        for _ in range(refined_samples_count):
+            samples.append(self._sample_from_space(self.refined_space))
+        
+        for _ in range(n - refined_samples_count):
+            samples.append(self._sample_from_space(self.space))
+
+        return samples
