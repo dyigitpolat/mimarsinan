eb1581925d27b24e247fbf104db0be0fc3f88096
yigit polat <dyigit96@gmail.com>
2023-06-13 23:59:15 +0800

    refactor search classes

--- src/mimarsinan/search/basic_architecture_searcher.py
+++ src/mimarsinan/search/basic_architecture_searcher.py
@@ -16,6 +16,14 @@ class BasicArchitectureSearcher:
     
     def _update_sampler(self, metrics):
         raise NotImplementedError
+    
+    def _evaluate_configurations(self, configurations):
+        metrics = []
+        for configuration in configurations:
+            if self._validate_configuration(configuration):
+                metrics.append(
+                    (configuration, self._evaluate_architecture(configuration)))
+        return metrics
 
     def get_optimized_model(self):
         cycles = 10
@@ -23,11 +31,7 @@ class BasicArchitectureSearcher:
         for _ in range(cycles):
             configurations = self._sample_configurations(configuration_batch_size)
 
-            metrics = []
-            for configuration in configurations:
-                if self._validate_configuration(configuration):
-                    metrics.append(
-                        (configuration, self._evaluate_architecture(configuration)))
+            metrics = self._evaluate_configurations(configurations)
             
             self._update_sampler(metrics)
             best_configuration = max(metrics, key = lambda x: x[1])
