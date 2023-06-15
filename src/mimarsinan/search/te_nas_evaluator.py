e9c3d4fa9745e8dac100e2a9953ff23c120c2655
yigit polat <dyigit96@gmail.com>
2023-06-15 23:52:07 +0800

    reuse trainer (and dataloader)

--- src/mimarsinan/search/te_nas_evaluator.py
+++ src/mimarsinan/search/te_nas_evaluator.py
@@ -1,26 +1,28 @@
-from mimarsinan.model_evaluation.te_nas_utils import get_ntk_n
+from mimarsinan.model_evaluation.te_nas_utils import get_ntk
 from mimarsinan.model_training.basic_trainer import BasicTrainer
 
+import torch.nn as nn
 class TE_NAS_Evaluator:
     def __init__(self, data_provider, loss, lr, device):
         self.data_provider = data_provider
         self.loss = loss
         self.lr = lr
         self.device = device
-
-    def evaluate(self, model):
-        trainer = BasicTrainer(
-            model, 
+        self.trainer = BasicTrainer(
+            nn.Identity(), 
             self.device, self.data_provider,
             self.loss)
+        self.input_data, _ = next(iter(data_provider.get_validation_loader(
+            data_provider.get_validation_set_size(),
+        )))
+
+    def evaluate(self, model):
+        self.trainer.model = model.to(self.device)
+        self.trainer.train_one_step(self.lr)
         
-        trainer.train_one_step(self.lr)
-        
-        ntk_score = get_ntk_n(
-            self.data_provider.get_validation_loader(
-                self.data_provider.get_validation_batch_size()
-            ), 
-            [model],
+        ntk_score = get_ntk(
+            self.input_data.to(self.device),
+            model,
             self.device)[0]
         
         return 1.0 / ntk_score
