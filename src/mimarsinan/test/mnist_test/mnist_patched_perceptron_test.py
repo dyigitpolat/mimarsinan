ab1bc92f1724d4cbae0263998f87b6c48f86e7cf
yigit polat <dyigit96@gmail.com>
2023-07-04 05:46:12 +0800

    modernize mnist experiment

--- src/mimarsinan/test/mnist_test/mnist_patched_perceptron_test.py
+++ src/mimarsinan/test/mnist_test/mnist_patched_perceptron_test.py
@@ -1,6 +1,8 @@
 from mimarsinan.test.mnist_test.mnist_test_utils import *
 from mimarsinan.common.wandb_utils import *
 from mimarsinan.pipelining.basic_classification_pipeline import BasicClassificationPipeline
+from mimarsinan.pipelining.deployment_pipeline import DeploymentPipeline
+
 from mimarsinan.data_handling.data_providers.mnist_data_provider import MNIST_DataProvider
 
 import torch
@@ -10,25 +12,27 @@ def test_mnist_patched_perceptron():
 
     reporter = WandB_Reporter("mnist_pipeline_test", "experiment")
 
-    pipeline = BasicClassificationPipeline(
+    pipeline = DeploymentPipeline(
         mnist_data_provider,
-        {
-            "max_axons": 256, 
-            "max_neurons": 256, 
-            "target_tq": 32, 
+        deployment_parameters = {
+            "lr": 0.001,
+            "pt_epochs": 10,
+            "aq_epochs": 10,
+            "wq_epochs": 10,
+            "nas_cycles": 2,
+            "nas_batch_size": 2
+        },
+        platform_constraints = {
+            "max_axons": 256,
+            "max_neurons": 256,
+            "target_tq": 32,
             "simulation_steps": 32,
             "weight_bits": 4
         },
-        {
-            "lr": 0.001, 
-            "pretraining_epochs": 10, 
-            "aq_cycle_epochs": 10, 
-            "wq_cycle_epochs": 10,
-            "aq_cycles": 10,
-            "wq_cycles": 10
-        },
-        reporter,
-        "../generated/mnist2/"
+        reporter = reporter,
+        working_directory = "../generated/mnist3/"
     )
 
     pipeline.run()
+    # pipeline.load_cache()
+    # pipeline.run_from("Pretraining")
