615ebda2dfaf169afe76bde9686b0d989029f0fd
yigit polat <dyigit96@gmail.com>
2023-07-04 05:45:01 +0800

    implement pipeline steps

--- src/mimarsinan/pipelining/pipeline_step.py
+++ src/mimarsinan/pipelining/pipeline_step.py
@@ -1,13 +1,15 @@
 class PipelineStep:
-    def __init__(self, requires, promises, pipeline):
+    def __init__(self, requires, promises, clears, pipeline):
         self.requires = requires
         self.promises = promises
+        self.clears = clears
         self.pipeline = pipeline
 
     def run(self):
         assert all([requirement in self.pipeline.cache for requirement in self.requires])
         self.process()
         assert all([promise in self.pipeline.cache for promise in self.promises])
+        assert all([entry not in self.pipeline.cache for entry in self.clears])
 
     def process(self):
         raise NotImplementedError
