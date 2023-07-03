2bec08f0a757eda49655c8d18a99290007583d3e
yigit polat <dyigit96@gmail.com>
2023-07-04 00:00:20 +0800

    pipeline step

---
+++ src/mimarsinan/pipelining/pipeline_step.py
@@ -0,0 +1,14 @@
+class PipelineStep:
+    def __init__(self, requires, promises, pipeline):
+        self.requires = requires
+        self.promises = promises
+        self.pipeline = pipeline
+
+    def run(self):
+        assert all([requirement in self.pipeline.cache for requirement in self.requires])
+        self.process()
+        assert all([promise in self.pipeline.cache for promise in self.promises])
+
+    def process(self):
+        raise NotImplementedError
+
