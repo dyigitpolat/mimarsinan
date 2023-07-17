cbfa7e819de6078e3613c6a3b41c0c43bc8e03f0
yigit polat <dyigit96@gmail.com>
2023-07-17 16:38:59 +0800

    use fused perceptron for weight quantization

--- src/mimarsinan/pipelining/pipeline_steps/weight_quantization_step.py
+++ src/mimarsinan/pipelining/pipeline_steps/weight_quantization_step.py
@@ -1,23 +1,24 @@
 from mimarsinan.pipelining.pipeline_step import PipelineStep
 
-from mimarsinan.tuning.tuners.weight_quantization_tuner import WeightQuantizationTuner 
+from mimarsinan.tuning.tuners.weight_quantization_tuner import WeightQuantizationTuner
+from mimarsinan.models.core_flow import CoreFlow 
 
 class WeightQuantizationStep(PipelineStep):
     def __init__(self, pipeline):
-        requires = ["nf_model", "nf_accuracy"]
+        requires = ["pf_model", "pf_accuracy"]
         promises = ["wq_model", "wq_accuracy"]
-        clears = ["nf_model"]
+        clears = ["pf_model"]
         super().__init__(requires, promises, clears, pipeline)
 
     def process(self):
+
         tuner = WeightQuantizationTuner(
             self.pipeline,
-            model = self.pipeline.cache['nf_model'],
+            model = self.pipeline.cache["pf_model"],
             quantization_bits = self.pipeline.config['weight_bits'],
             target_tq = self.pipeline.config['target_tq'],
             target_accuracy = self.pipeline.cache['nf_accuracy'] * 0.99,
             lr = self.pipeline.config['lr'])
-        
         validation_accuracy = tuner.run()
 
         assert validation_accuracy > self.pipeline.cache['nf_accuracy'] * 0.9, \
@@ -26,4 +27,4 @@ class WeightQuantizationStep(PipelineStep):
         self.pipeline.cache.add("wq_model", tuner.model, 'torch_model')
         self.pipeline.cache.add("wq_accuracy", validation_accuracy)
 
-        self.pipeline.cache.remove("nf_model")
+        self.pipeline.cache.remove("pf_model")
