a0022674997827a652cb93392bad2129aa66f85a
dyigitpolat <yigit@u.nus.edu>
2023-07-19 04:18:11 +0800

    redactor basic adaptation class

--- src/mimarsinan/tuning/basic_smooth_adaptation.py
+++ src/mimarsinan/tuning/basic_smooth_adaptation.py
@@ -1,10 +1,11 @@
 from mimarsinan.tuning.basic_interpolation import BasicInterpolation
 
 class BasicSmoothAdaptation:
-    def __init__(self, adaptation_function):
+    def __init__(self, adaptation_function, interpolators):
         self.adaptation_function = adaptation_function
+        self.interpolators = interpolators
 
-    def adapt_smoothly(self, interpolators, cycles):
+    def adapt_smoothly(self, cycles):
         for cycle in range(cycles):
             t = (cycle + 1) / cycles
-            self.adaptation_function(*[i(t) for i in interpolators])
+            self.adaptation_function(*[i(t) for i in self.interpolators])
