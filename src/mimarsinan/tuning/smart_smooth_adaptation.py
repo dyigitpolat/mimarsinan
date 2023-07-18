9595cf83974e9a8671bdfab3342fd27634c6724d
dyigitpolat <yigit@u.nus.edu>
2023-07-19 04:18:39 +0800

    simplify smart adaptation using target adjuster

--- src/mimarsinan/tuning/smart_smooth_adaptation.py
+++ src/mimarsinan/tuning/smart_smooth_adaptation.py
@@ -1,55 +1,40 @@
 from mimarsinan.tuning.basic_smooth_adaptation import BasicSmoothAdaptation
+from mimarsinan.tuning.adaptation_target_adjuster import AdaptationTargetAdjuster
 
 class SmartSmoothAdaptation(BasicSmoothAdaptation):
     def __init__(
         self, adaptation_function, 
         state_clone_function, state_restore_function,
-        evaluation_function):
+        evaluation_function, interpolators):
 
-        super().__init__(adaptation_function)
+        super().__init__(adaptation_function, interpolators)
         self.state_clone_function = state_clone_function
         self.state_restore_function = state_restore_function
         self.evaluation_function = evaluation_function
         self.min_step = 0.0001
-
         self.tolerance = 0.01
-        self.target_decay = 0.999
 
-        self.target_metric = 1.0
-        self.original_metric = 1.0
+        self.original_target = \
+            self.evaluation_function(*[i(0) for i in self.interpolators]) * (1.0 - self.tolerance)
+        self.target_adjuster = AdaptationTargetAdjuster(self.original_target)
 
     def _adjust_minimum_step(self, step_size, t):
-        if step_size < self.min_step:
+        halfway = (1 - t) / 2
+        if step_size < self.min_step and self.min_step < halfway:
             self.min_step *= 2.0
 
-    def _update_target(self, current_metric):
-        decayed_target_metric = self.target_metric * self.target_decay
-        promoted_current_metric = current_metric
-
-        if current_metric > self.target_metric:
-            promoted_current_metric = max(
-                current_metric,
-                0.1 * self.original_metric + 0.9 * current_metric
-            )
-
-        self.target_metric = max(
-            decayed_target_metric, 
-            promoted_current_metric)
-        
-        print("target_metric: ", self.target_metric)
-
-    def _find_step_size(self, t, interpolators):
+    def _find_step_size(self, t):
         step_size = (1 - t) * 2
         state = self.state_clone_function()
 
         current_metric = 0
-        while current_metric < self.target_metric and step_size > self.min_step:
+        while current_metric < self.target_adjuster.get_target() and step_size > self.min_step:
             step_size /= 2
             print("step_size: ", step_size)
             
             next_t = t + step_size
-            current_metric = self.evaluation_function(*[i(next_t) for i in interpolators])
-            self._update_target(current_metric)
+            current_metric = self.evaluation_function(*[i(next_t) for i in self.interpolators])
+            self.target_adjuster.update_target(current_metric)
             self.state_restore_function(state)
 
         self._adjust_minimum_step(step_size, t)
@@ -57,18 +42,12 @@ class SmartSmoothAdaptation(BasicSmoothAdaptation):
         print("current_metric: ", current_metric)
         return step_size
 
-    def adapt_smoothly(self, interpolators, max_cycles = None):
-        state = self.state_clone_function()
-        self.target_metric = \
-            self.evaluation_function(*[i(0) for i in interpolators]) * (1 - self.tolerance)
-        self.original_metric = self.target_metric
-        self.state_restore_function(state)
-
+    def adapt_smoothly(self, max_cycles = None):
         t = 0
         cycles = 0
         while t < 1 and (not max_cycles or cycles < max_cycles):
-            step_size = self._find_step_size(t, interpolators)
+            step_size = self._find_step_size(t)
             t += step_size
-            self.adaptation_function(*[i(t) for i in interpolators])
+            self.adaptation_function(*[i(t) for i in self.interpolators])
             cycles += 1
 
