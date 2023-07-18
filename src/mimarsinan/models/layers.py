2a05646bdbc903776fd1854f14c81484e4ab9541
dyigitpolat <yigit@u.nus.edu>
2023-07-18 20:21:01 +0800

    remove unnecessary classes

--- src/mimarsinan/models/layers.py
+++ src/mimarsinan/models/layers.py
@@ -1,43 +1,5 @@
 import torch.nn as nn
 import torch
-
-class Normalizer(nn.Module):
-    def __init__(self):
-        super(Normalizer, self).__init__()
-
-        self.eps = torch.tensor(1e-8)
-        self.running_mean = nn.Parameter(torch.tensor(0.0), requires_grad=False)
-        self.momentum = torch.tensor(0.1)
-
-    def get_factor(self):
-        return (1.0 / (self.running_mean + self.eps)) ** 0.5
-
-    def forward(self, x):
-        abs_mean = torch.mean(x**2)
-
-        m = self.momentum
-        self.running_mean = nn.Parameter(
-            (1 - m)*self.running_mean + m*abs_mean, requires_grad=False)
-        
-        factor = self.get_factor().detach()
-        return x * factor
-    
-class WokeBatchNorm1d(nn.BatchNorm1d):
-    def __init__(self, 
-                 num_features, eps=1e-5, momentum=0.1, affine=True, 
-                 track_running_stats=True):
-        super(WokeBatchNorm1d, self).__init__(
-            num_features, eps, momentum, affine, track_running_stats)
-        
-    def forward(self, x):
-        if len(x.shape) == 2:
-            return nn.BatchNorm1d.forward(self, x)
-        else:
-            x = x.transpose(1, 2)
-            x = nn.BatchNorm1d.forward(self, x)
-            x = x.transpose(1, 2)
-            return x
-    
     
 from torch.autograd import Function
 
