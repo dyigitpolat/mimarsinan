01f4600be4af1d41431a22e8e0f67dd4b69d3cd3
dyigitpolat <yigit@u.nus.edu>
2023-07-20 17:19:48 +0800

    simplify chip quantization

--- src/mimarsinan/transformations/chip_quantization.py
+++ src/mimarsinan/transformations/chip_quantization.py
@@ -1,27 +1,23 @@
 from mimarsinan.transformations.weight_quantization import TensorQuantization
-from mimarsinan.transformations.weight_clipping import SoftTensorClipping
 import numpy as np
 
 class ChipQuantization:
     def __init__(self, bits):
         self.quantizer = TensorQuantization(bits)
-        self.target_minimum_threshold = self.quantizer.q_max
-    
-    def calculate_core_thresholds(self, cores):
-        for core in cores:
-            core.threshold = self.quantizer.q_max
-    
-    def quantize(self, cores):
-        self.calculate_core_thresholds(cores)
 
+    def unscaled_quantize(self, cores):
+        for core in cores:
+            core.core_matrix = self.quantizer.quantize(core.core_matrix)
+            
+    def quantize(self, cores):
         for core in cores:
             threshold_scale = np.max(np.abs(core.core_matrix))
+
             core.core_matrix /= threshold_scale
             core.core_matrix = self.quantizer.scaled_quantize(core.core_matrix)
             
-            assert np.max(np.abs(core.core_matrix)) <= self.quantizer.q_max, \
+            assert np.max(np.abs(core.core_matrix)) == self.quantizer.q_max, \
                 f"{np.max(np.abs(core.core_matrix))} > {self.quantizer.q_max}"
             
             core.threshold /= threshold_scale
-
-        return 1.0
+            
