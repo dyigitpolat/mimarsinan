65a8b9a0f354460ff88bebc68b45bece840c8c6c
dyigitpolat <yigit@u.nus.edu>
2023-07-20 17:17:42 +0800

    perceptron threshold aware tuning

--- src/mimarsinan/tuning/tuners/activation_quantization_tuner.py
+++ src/mimarsinan/tuning/tuners/activation_quantization_tuner.py
@@ -33,15 +33,22 @@ class ActivationQuantizationTuner(BasicTuner):
             lambda p: torch.clamp(p, -1, 1) ])
 
     def _update_and_evaluate(self, rate):
-        self.model.set_activation(CQ_Activation_Parametric(self.target_tq, rate, self.base_activation))
-        self.trainer.weight_transformation = self._mixed_transform(rate)
+        for perceptron in self.model.get_perceptrons():
+            perceptron.set_activation(CQ_Activation_Parametric(
+                self.target_tq, 
+                rate, 
+                self.base_activation, 
+                perceptron.base_threshold))
+        
         self.trainer.train_n_epochs(self._find_lr() / 2, 1)
         return self.trainer.validate()
 
     def run(self):
         super().run()
+        for perceptron in self.model.get_perceptrons():
+            perceptron.set_activation(
+                CQ_Activation(self.target_tq, perceptron.base_threshold))
         
-        self.model.set_activation(CQ_Activation(self.target_tq))
         self.trainer.weight_transformation = self._get_new_parameter_transform()
         self.trainer.train_until_target_accuracy(self._find_lr() / 2, self.epochs, self._get_target())
 
