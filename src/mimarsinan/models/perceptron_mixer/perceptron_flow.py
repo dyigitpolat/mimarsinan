c8c4f690ca131fb31de34c7c59453924562f4662
dyigitpolat <yigit@u.nus.edu>
2023-07-29 22:35:00 +0800

    refactor normalization fusion

--- src/mimarsinan/models/perceptron_mixer/perceptron_flow.py
+++ src/mimarsinan/models/perceptron_mixer/perceptron_flow.py
@@ -3,10 +3,6 @@ import torch.nn as nn
 class PerceptronFlow(nn.Module):
     def __init__(self):
         super(PerceptronFlow, self).__init__()
-
-    def fuse_normalization(self):
-        for layer in self.get_perceptrons():
-            layer.fuse_normalization()
     
     def get_perceptrons(self):
         raise NotImplementedError
