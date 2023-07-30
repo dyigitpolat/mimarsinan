76d680abe2779b44274e09f9d4fcac22a55c7c0d
dyigitpolat <yigit@u.nus.edu>
2023-07-31 03:53:17 +0800

    whitespace stuff

--- src/mimarsinan/search/small_step_evaluator.py
+++ src/mimarsinan/search/small_step_evaluator.py
@@ -2,6 +2,7 @@ from mimarsinan.model_training.basic_trainer import BasicTrainer
 from mimarsinan.models.layers import ClampedReLU, NoisyDropout
 
 import torch.nn as nn
+
 class SmallStepEvaluator:
     def __init__(self, data_provider, loss, lr, device):
         self.data_provider = data_provider
@@ -25,4 +26,3 @@ class SmallStepEvaluator:
 
         self.trainer.train_n_epochs(self.lr, 1)
         return self.trainer.validate()
-        
