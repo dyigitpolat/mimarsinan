afcee2c879177997cb05cdcf3cf829652392992e
dyigitpolat <yigit@u.nus.edu>
2023-07-31 23:58:48 +0800

    refine wq step

--- src/mimarsinan/tuning/tuners/weight_quantization_tuner.py
+++ src/mimarsinan/tuning/tuners/weight_quantization_tuner.py
@@ -2,8 +2,9 @@ from mimarsinan.tuning.tuners.basic_tuner import BasicTuner
 from mimarsinan.transformations.parameter_transforms.sequential_transform import SequentialTransform
 from mimarsinan.transformations.weight_quantization import TensorQuantization
 from mimarsinan.transformations.weight_clipping import SoftTensorClipping
+from mimarsinan.pipelining.pipeline_steps.perceptron_fusion_step import FusedLinear
 
-from mimarsinan.models.layers import CQ_Activation
+from mimarsinan.models.layers import CQ_Activation, ScaleActivation
 
 import torch.nn as nn
 import torch
@@ -22,6 +23,10 @@ class WeightQuantizationTuner(BasicTuner):
         self.target_tq = target_tq
         self.quantization_bits = quantization_bits
 
+        from mimarsinan.visualization.activation_function_visualization import ActivationFunctionVisualizer
+        for idx, perceptron in enumerate(self.model.get_perceptrons()):
+            ActivationFunctionVisualizer(perceptron.activation, -3, 3, 0.001).plot(f"./generated/_wqt{idx}.png")
+
     def _get_target_decay(self):
         return 0.999
     
@@ -31,15 +36,13 @@ class WeightQuantizationTuner(BasicTuner):
     def _get_new_parameter_transform(self):
         return SequentialTransform([ 
             lambda p: torch.clamp(p, -1, 1),
-            SoftTensorClipping(0.1).get_clipped_weights,
+            SoftTensorClipping(0.01).get_clipped_weights,
             TensorQuantization(self.quantization_bits).quantize])
 
     def _update_and_evaluate(self, rate):
-        for perceptron in self.model.get_perceptrons():
-            perceptron.set_activation(
-                CQ_Activation(self.target_tq, perceptron.base_threshold))
-            
-        self.trainer.train_one_step(self.pipeline_lr / 40)
+        self.trainer.weight_transformation = self._mixed_transform(rate)
+        self.trainer.train_one_step(self._find_lr())
+
         return self.trainer.validate()
 
     def run(self):
