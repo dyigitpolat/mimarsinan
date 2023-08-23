6efd5d6ef0c5db9b4d667a2d7794aba9ca97f57e
dyigitpolat <yigit@u.nus.edu>
2023-08-23 16:06:39 +0800

    assert all required entries are accessed + details

--- src/mimarsinan/pipelining/pipeline_step.py
+++ src/mimarsinan/pipelining/pipeline_step.py
@@ -5,13 +5,23 @@ class PipelineStep:
         self.clears = clears
         self.pipeline = pipeline
 
+        self._accessed_entries = set()
+
     def run(self):
-        assert all([requirement in self.pipeline.cache for requirement in self.requires])
+        assert all([requirement in self.pipeline.cache for requirement in self.requires]), \
+            f"Pipeline error: Some requirements are not found in the cache."
+
+        self._accessed_entries = set()
         self.process()
         for entry in self.clears:
             self.pipeline.cache.remove(entry)
-        assert all([promise in self.pipeline.cache for promise in self.promises])
-        assert all([entry not in self.pipeline.cache for entry in self.clears])
+
+        assert all([requirement in self._accessed_entries for requirement in self.requires]), \
+            f"Pipeline error: Some required entries ({set(self.requires) - self._accessed_entries}) were not accessed."
+        assert all([promise in self.pipeline.cache for promise in self.promises]), \
+            f"Pipeline error: Some promised entries were not added."
+        assert all([entry not in self.pipeline.cache for entry in self.clears]), \
+            f"Pipeline error: Some cleared entries were not removed."
 
     def process(self):
         raise NotImplementedError
@@ -20,10 +30,11 @@ class PipelineStep:
         raise NotImplementedError
     
     def get_entry(self, key):
-        assert key in self.requires, f"You cannot retrieve a non-required entry ({key}) from the cache."
+        assert key in self.requires, f"A non-required entry ({key}) cannot be retrieved from the cache."
+        self._accessed_entries.add(key)
         return self.pipeline.cache[key]
     
     def add_entry(self, key, object, load_store_strategy = "basic"):
-        assert key in self.promises, f"You cannot add a non-promised entry ({key}) to the cache."
+        assert key in self.promises, f"A non-promised entry ({key}) cannot be added to the cache."
         self.pipeline.cache.add(key, object, load_store_strategy)
 
