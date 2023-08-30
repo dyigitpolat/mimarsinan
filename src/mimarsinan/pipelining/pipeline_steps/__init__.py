6796d94928a03654674355c79aca5614f952477d
dyigitpolat <yigit@u.nus.edu>
2023-08-30 14:14:04 +0800

    implement vanilla pipeline

--- src/mimarsinan/pipelining/pipeline_steps/__init__.py
+++ src/mimarsinan/pipelining/pipeline_steps/__init__.py
@@ -13,3 +13,4 @@ from .clamp_adaptation_step import ClampAdaptationStep
 from .perceptron_fusion_step import PerceptronFusionStep
 from .scale_adaptation_step import ScaleAdaptationStep
 from .model_building_step import ModelBuildingStep
+from .vanilla_model_configuration import VanillaModelConfigurationStep
