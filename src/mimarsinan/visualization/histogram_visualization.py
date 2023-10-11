f9452b506733a454118769b431d8a8548ad74327
dyigitpolat <yigit@u.nus.edu>
2023-10-11 22:33:53 +0800

    vertical line support for activation visualiation

--- src/mimarsinan/visualization/histogram_visualization.py
+++ src/mimarsinan/visualization/histogram_visualization.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 
 class HistogramVisualizer:
-    def __init__(self, hist, bin_edges, min_x = -1.5, max_x = 1.5, device = 'cpu'):
+    def __init__(self, hist, bin_edges, min_x = -1.5, max_x = 1.5, v_line = None, device = 'cpu'):
         self.hist = hist
         self.bin_edges = bin_edges
         self.max_x = bin_edges[-1]
         self.min_x = bin_edges[0]
+        self.v_line = v_line
 
     def plot(self, filename):
         plt.grid()
@@ -14,6 +15,9 @@ class HistogramVisualizer:
         plt.ylim(max(0, min(self.hist) - 0.1), max(self.hist) + 0.1)
 
         plt.bar(self.bin_edges[:-1], self.hist, width=self.bin_edges[1] - self.bin_edges[0], align="edge")
+        if self.v_line is not None:
+            plt.axvline(x=self.v_line, color='r')
+            
         plt.xlabel('Activation Value')
         plt.ylabel('Count')
         plt.title('Activation Histogram')
