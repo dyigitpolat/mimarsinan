f009f7bedf1830d710bb0c2e432bf399ffa6dea7
dyigitpolat <yigit@u.nus.edu>
2023-11-28 18:43:59 +0800

    fix quantization with scale

--- src/mimarsinan/transformations/normalization_aware_perceptron_quantization.py
+++ src/mimarsinan/transformations/normalization_aware_perceptron_quantization.py
@@ -15,7 +15,6 @@ class NormalizationAwarePerceptronQuantization:
         self.rate = rate
     
     def transform(self, perceptron):
-        out_perceptron = copy.deepcopy(perceptron)
         w = PerceptronTransformer().get_effective_weight(perceptron)
         b = PerceptronTransformer().get_effective_bias(perceptron)
         
@@ -23,22 +22,15 @@ class NormalizationAwarePerceptronQuantization:
         b_max = torch.max(torch.abs(b))
         p_max = max(w_max, b_max)
 
-        natural_scale = 1.0
-        target_scale = 1.0 / p_max
-        adjusted_scale = target_scale * self.rate + natural_scale * (1.0 - self.rate)
-
-        adjusted_scale_correction = 1.0 * self.rate + adjusted_scale * (1.0 - self.rate)
-        adjusted_clamp = max(1.0, 1.0 * self.rate + p_max * (1.0 - self.rate))
+        scale = self.q_max * (1.0 / p_max)
+        perceptron.set_parameter_scale(scale)
         def quantize_param(param):
-            scaled_param = param * adjusted_scale
-            clipped_param = torch.clamp(scaled_param, -adjusted_clamp, adjusted_clamp)
-            #clipped_param = torch.clamp(param, -adjusted_clamp, adjusted_clamp)
-            return torch.round(clipped_param * self.q_max) / (self.q_max * adjusted_scale_correction)
+            scaled_param = param * scale
+            quantized_param = torch.round(scaled_param)
+            rescaled_param = quantized_param / (scale)
+            return rescaled_param
         
-        PerceptronTransformer().apply_effective_parameter_transform(out_perceptron, quantize_param) 
-
-        #self._verify_fuse_quantization(out_perceptron)
-        return out_perceptron.to(self.device)
+        PerceptronTransformer().apply_effective_parameter_transform(perceptron, quantize_param) 
 
     def _verify_fuse_quantization(self, perceptron):
         perceptron.to(self.device)
