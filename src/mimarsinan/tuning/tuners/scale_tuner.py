552d10f4185002cc5740da49ae3f6e98c4bf6e7d
dyigitpolat <yigit@u.nus.edu>
2023-11-28 18:46:18 +0800

    simplify scale tuner

--- src/mimarsinan/tuning/tuners/scale_tuner.py
+++ src/mimarsinan/tuning/tuners/scale_tuner.py
@@ -20,8 +20,9 @@ class ScaleTuner(BasicTuner):
 
         self.lr = lr
         self.adaptation_manager = adaptation_manager
-        self.previous_scales = [
+        self.original_scales = [
             perceptron.activation_scale for perceptron in self.model.get_perceptrons()]
+        self.previous_rates = [ 0.0 for _ in self.original_scales ]
 
     def _get_target_decay(self):
         return 0.99
@@ -32,22 +33,26 @@ class ScaleTuner(BasicTuner):
     def _get_new_parameter_transform(self):
         return lambda x: x
     
-    def _update_parameters(self, perceptron, prev_act_scale, new_act_scale):
-        scale = new_act_scale / prev_act_scale
-        PerceptronTransformer().apply_effective_parameter_transform(perceptron, lambda x: x * scale)
+    def _update_parameters(self, perceptron, original_scale, prev_rate, rate):
+        # prev_scale = original_scale * (1.0 - prev_rate) + 1.0 * prev_rate
+        new_scale = original_scale * (1.0 - rate) + 1.0 * rate
+        # scale_factor = new_scale / prev_scale
         
-        if not isinstance(perceptron.normalization, nn.Identity):
-            perceptron.normalization.running_mean *= scale
-            perceptron.normalization.running_var *= (scale ** 2)
+        # if not isinstance(perceptron.normalization, nn.Identity):
+        #     perceptron.normalization.running_mean *= scale_factor
+        #     perceptron.normalization.running_var *= (scale_factor ** 2)
+        
+        perceptron.set_activation_scale(max(1.0, new_scale))
+        #PerceptronTransformer().apply_effective_parameter_transform(perceptron, lambda x: x * scale_factor)
 
     def _update_and_evaluate(self, rate):
-        for perceptron, prev_scale in zip(self.model.get_perceptrons(), self.previous_scales):
-            target_scale = 1.0
-            new_scale = target_scale * rate + prev_scale * (1.0 - rate)
-            perceptron.activation_scale = new_scale
+        for idx, perceptron in enumerate(self.model.get_perceptrons()):
+            prev_rate, original_scale = self.previous_rates[idx], self.original_scales[idx]
 
-            self._update_parameters(perceptron, prev_scale, perceptron.activation_scale)
+            self._update_parameters(perceptron, original_scale, prev_rate, rate)
             self.adaptation_manager.update_activation(self.pipeline.config, perceptron)
+
+            self.previous_rates[idx] = rate
 
         self.trainer.train_one_step(self._find_lr())
         return self.trainer.validate()
