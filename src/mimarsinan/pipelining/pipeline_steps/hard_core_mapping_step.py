f61094b5d8fee64424bc3b88a903d7cbae6724a7
dyigitpolat <yigit@u.nus.edu>
2023-12-13 21:52:27 +0800

    test hard core mapping

--- src/mimarsinan/pipelining/pipeline_steps/hard_core_mapping_step.py
+++ src/mimarsinan/pipelining/pipeline_steps/hard_core_mapping_step.py
@@ -3,6 +3,10 @@ from mimarsinan.pipelining.pipeline_step import PipelineStep
 from mimarsinan.mapping.softcore_mapping import HardCoreMapping
 from mimarsinan.visualization.hardcore_visualization import HardCoreMappingVisualizer
 
+from mimarsinan.model_training.basic_trainer import BasicTrainer
+from mimarsinan.data_handling.data_loader_factory import DataLoaderFactory
+from mimarsinan.models.spiking_core_flow import SpikingCoreFlow
+
 class HardCoreMappingStep(PipelineStep):
 
     def __init__(self, pipeline):
@@ -28,5 +32,13 @@ class HardCoreMappingStep(PipelineStep):
         HardCoreMappingVisualizer(hard_core_mapping).visualize(
             self.pipeline.working_directory + "/hardcore_mapping.png"
         )
+
+        print("Hard Core Mapping Test:", BasicTrainer(
+            SpikingCoreFlow(
+                self.pipeline.config["input_shape"], 
+                hard_core_mapping, 
+                self.pipeline.config["simulation_steps"]), 
+            self.pipeline.config["device"], 
+            DataLoaderFactory(self.pipeline.data_provider_factory), None).test())
 
         self.add_entry("hard_core_mapping", hard_core_mapping, 'pickle')
