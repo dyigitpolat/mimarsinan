ef7085e266bfdd2853a11f34a89683232328582e
dyigitpolat <yigit@u.nus.edu>
2023-12-21 23:47:42 +0800

    input cq during act q

--- src/mimarsinan/tuning/tuners/activation_quantization_tuner.py
+++ src/mimarsinan/tuning/tuners/activation_quantization_tuner.py
@@ -1,5 +1,6 @@
 from mimarsinan.tuning.tuners.perceptron_tuner import PerceptronTuner
 from mimarsinan.transformations.perceptron_transformer import PerceptronTransformer
+from mimarsinan.models.layers import TransformedActivation, DecoratedActivation, RandomMaskAdjustmentStrategy, RateAdjustedDecorator, ClampDecorator, QuantizeDecorator
 
 import torch
 
@@ -20,9 +21,10 @@ class ActivationQuantizationTuner(PerceptronTuner):
 
         self.target_tq = target_tq
         self.adaptation_manager = adaptation_manager
+        self.base_input_activation = model.input_activation
 
     def _get_target_decay(self):
-        return 0.999
+        return 0.99
     
     def _get_previous_perceptron_transform(self, rate):
         return lambda p: None
@@ -31,6 +33,17 @@ class ActivationQuantizationTuner(PerceptronTuner):
         return lambda p: None
 
     def _update_and_evaluate(self, rate):
+        self.model.input_activation = TransformedActivation(
+            base_activation = self.base_input_activation,
+            decorators = [
+                ClampDecorator(torch.tensor(0.0), torch.tensor(1.0)),
+                RateAdjustedDecorator(
+                    rate,
+                    QuantizeDecorator(torch.tensor(self.pipeline.config["target_tq"]), torch.tensor(1.0)),
+                    RandomMaskAdjustmentStrategy()
+                )
+            ])
+        
         self.adaptation_manager.quantization_rate = rate
         for perceptron in self.model.get_perceptrons():
             self.adaptation_manager.update_activation(self.pipeline.config, perceptron)
