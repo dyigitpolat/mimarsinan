c9203ae76a948d03a66f857947b42764c9400ba8
dyigitpolat <dyigit96@gmail.com>
2023-12-28 11:20:02 +0800

    support input activations

--- src/mimarsinan/models/perceptron_mixer/perceptron.py
+++ src/mimarsinan/models/perceptron_mixer/perceptron.py
@@ -16,11 +16,13 @@ class Perceptron(nn.Module):
             input_features, output_channels, bias=bias)
 
         self.normalization = normalization
+        self.input_activation = nn.Identity()
         self.activation = nn.LeakyReLU()
 
         self.regularization = nn.Identity()
         
         self.parameter_scale = nn.Parameter(torch.tensor(1.0), requires_grad=False)
+        self.input_activation_scale = nn.Parameter(torch.tensor(1.0), requires_grad=False)
         self.activation_scale = nn.Parameter(torch.tensor(1.0), requires_grad=False)
 
         self.base_activation = nn.LeakyReLU()
@@ -34,7 +36,12 @@ class Perceptron(nn.Module):
         if isinstance(new_scale, float):
             new_scale = torch.tensor(new_scale)
         self.activation_scale.data = new_scale.data
-
+    
+    def set_input_activation_scale(self, new_scale):
+        if isinstance(new_scale, float):
+            new_scale = torch.tensor(new_scale)
+        self.input_activation_scale.data = new_scale.data
+        
     def set_activation(self, activation):
         self.activation = activation
 
@@ -42,6 +49,8 @@ class Perceptron(nn.Module):
         self.regularization = regularizer
 
     def forward(self, x):
+        x = self.input_activation(x)
+        
         out = self.layer(x)
         out = self.normalization(out)
         out = self.activation(out)
