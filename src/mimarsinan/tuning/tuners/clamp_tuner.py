c3a7f8866707cd69aaa49a2cb654ef15c2082400
dyigitpolat <dyigit96@gmail.com>
2024-02-06 00:47:25 +0800

    adjustments in clamp tuna

--- src/mimarsinan/tuning/tuners/clamp_tuner.py
+++ src/mimarsinan/tuning/tuners/clamp_tuner.py
@@ -40,15 +40,16 @@ class ClampTuner(PerceptronTuner):
 
         validator.validate()
 
+        max_target_scale = 0.0
         for perceptron in self.model.get_perceptrons():
             saved_tensor_dec = perceptron.activation.pop_decorator()
             in_min = saved_tensor_dec.latest_input.min()
             in_max = saved_tensor_dec.latest_input.max()
             x = saved_tensor_dec.latest_input
             
-            bins = 100
+            bins = 1000
             activation_hist = torch.histc(x.flatten(), bins=bins, min=in_min.item(), max=in_max.item())
-            bin_edges = torch.linspace(in_min.item(), in_max.item(), steps=bins+1)
+            bin_edges = torch.linspace(in_min.item(), in_max.item(), steps=bins+1).to(self.pipeline.config['device'])
 
             activation_hist *= bin_edges[1:].to(self.pipeline.config['device'])
             activation_hist[activation_hist < 0] = 0
@@ -56,14 +57,16 @@ class ClampTuner(PerceptronTuner):
             cumulative_hist = activation_hist.cumsum(0)
             cumulative_hist /= hist_sum
 
-            clip_rate = 0.8
+            clip_rate = 0.999
             
             # # find the index of the bin which first exceeds the rate
-            index = (cumulative_hist > clip_rate).flatten().nonzero()[0]
-            act_scale = bin_edges[index].item()
+            index = (cumulative_hist > clip_rate).flatten().nonzero()[0].to(self.pipeline.config['device'])
+            clipped_act_scale = bin_edges[index].item()
 
-            target_act_scale = act_scale * (1.0 - rate) + rate
+            target_act_scale = (in_max * (1.0 - rate) + rate * clipped_act_scale) 
+
             perceptron.set_activation_scale(target_act_scale)
+            max_target_scale = max(max_target_scale, target_act_scale)
 
     def _update_and_evaluate(self, rate):
         self._calculate_activation_scales(self.adaptation_manager, self.trainer, rate)
@@ -78,5 +81,4 @@ class ClampTuner(PerceptronTuner):
     def run(self):
         self.trainer.validate()
         super().run()
-        
         return self.trainer.validate()
