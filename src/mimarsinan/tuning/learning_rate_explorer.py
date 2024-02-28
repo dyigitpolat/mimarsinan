84332f83ba216f7ad45fb67b807f54b7f5bc8bd0
dyigitpolat <dyigit96@gmail.com>
2024-02-28 19:12:53 +0800

    fix state dict bug

--- src/mimarsinan/tuning/learning_rate_explorer.py
+++ src/mimarsinan/tuning/learning_rate_explorer.py
@@ -1,9 +1,17 @@
+import copy
 class LearningRateExplorer:
     def __init__(self, 
-                 trainer, model, max_lr, min_lr, 
-                 desired_improvement = 0.0, base_accuracy = None):
+        clone_state,
+        restore_state,
+        trainer, model, max_lr, min_lr, 
+        desired_improvement = 0.0, base_accuracy = None):
+
+        self.clone_state = clone_state
+        self.restore_state = restore_state
+
         self.training_function = trainer.train_one_step
         self.evaluation_function = trainer.validate
+
         self.model = model
         self.max_lr = max_lr
         self.min_lr = min_lr
@@ -11,23 +19,30 @@ class LearningRateExplorer:
         self.base_accuracy = base_accuracy
 
     def find_lr_for_tuning(self):
-        original_state = self.model.state_dict()
+        original_state = self.clone_state()
+        print("original acc:", self.evaluation_function())
 
         if self.base_accuracy is None:
             self.base_accuracy = self.evaluation_function()
 
-        real_improvement = self.desired_improvement * (1.0 - self.base_accuracy)
-        target_acc = self.base_accuracy + real_improvement
-
+        iters = 100
         lr = self.max_lr
-        acc = 0
-        while acc < target_acc and lr > self.min_lr:
-            self.training_function(lr)
-            acc = self.evaluation_function()
-            if acc < target_acc:
-                lr *= 0.7
-                real_improvement *= 0.999
-                target_acc = self.base_accuracy + real_improvement
-            
-            self.model.load_state_dict(original_state)
-        return lr
+        lrs = []
+        accs = []
+        while lr > self.min_lr:
+            acc = 0
+            iter = 0
+            while acc < self.base_accuracy and iter < iters:
+                self.training_function(lr)
+                acc = self.evaluation_function()
+                iter += 1
+
+            lrs.append(lr)
+            accs.append(acc)
+            lr *= 0.7
+            self.restore_state(original_state)
+
+        print (lrs)
+        print (accs)
+        best_lr = lrs[accs.index(max(accs))]       
+        return best_lr
