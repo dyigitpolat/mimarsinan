2db901c00fc89e198e110626fe9850e8ef4a6fa1
dyigitpolat <dyigit96@gmail.com>
2024-05-14 09:40:42 +0800

    fix quantization

--- src/mimarsinan/transformations/chip_quantization.py
+++ src/mimarsinan/transformations/chip_quantization.py
@@ -7,15 +7,21 @@ class ChipQuantization:
 
     def verify_quantization(self, core):
         scale = core.parameter_scale.cpu().numpy()
-        assert np.allclose(
-            core.core_matrix * scale, np.round(core.core_matrix * scale),
-            atol=1e-3, rtol=1e-3)
 
         assert np.max(np.round(core.core_matrix * scale)) <= self.quantizer.q_max, \
             f"{np.max(np.round(core.core_matrix * scale))} > {self.quantizer.q_max}"
         
         assert np.min(np.round(core.core_matrix * scale)) >= self.quantizer.q_min, \
             f"{np.min(np.round(core.core_matrix * scale))} < {self.quantizer.q_min}"
+        
+        #print(core.core_matrix * scale)
+        
+        assert np.allclose(
+            core.core_matrix * scale, np.round(core.core_matrix * scale),
+            atol=1e-3, rtol=1e-3)
+        
+        print("OK")
+
 
     def unscaled_quantize(self, cores):
         for core in cores:
@@ -23,9 +29,11 @@ class ChipQuantization:
             
     def quantize(self, cores):
         for core in cores:
+            print(core.parameter_scale)
+
+        for core in cores:
             self.verify_quantization(core)
             core.threshold = core.parameter_scale.item()
-            print(core.threshold)
-
+            
             core.core_matrix *= core.parameter_scale.item()
             core.core_matrix = np.round(core.core_matrix)
