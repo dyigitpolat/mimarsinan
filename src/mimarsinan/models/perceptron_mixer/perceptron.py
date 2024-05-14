30ea9018ed25ba7a6d3cb9ed32a99ed985782fe3
dyigitpolat <dyigit96@gmail.com>
2024-05-14 09:36:29 +0800

    implement scaler in perceptron

--- src/mimarsinan/models/perceptron_mixer/perceptron.py
+++ src/mimarsinan/models/perceptron_mixer/perceptron.py
@@ -1,3 +1,6 @@
+from mimarsinan.models.layers import MaxValueScaler
+
+
 import torch.nn as nn
 import torch
 
@@ -17,6 +20,11 @@ class Perceptron(nn.Module):
             input_features, output_channels, bias=bias)
 
         self.normalization = normalization
+
+        self.scaler = nn.Identity()
+        self.base_scaler = MaxValueScaler()
+        self.scale_factor = nn.Parameter(torch.tensor(1.0), requires_grad=False)
+
         self.input_activation = nn.Identity()
         self.activation = nn.LeakyReLU()
 
@@ -42,6 +50,11 @@ class Perceptron(nn.Module):
         if isinstance(new_scale, float):
             new_scale = torch.tensor(new_scale)
         self.input_activation_scale.data = new_scale.data
+
+    def set_scale_factor(self, new_scale):
+        if isinstance(new_scale, float):
+            new_scale = torch.tensor(new_scale)
+        self.scale_factor.data = new_scale.data
         
     def set_activation(self, activation):
         self.activation = activation
@@ -49,11 +62,16 @@ class Perceptron(nn.Module):
     def set_regularization(self, regularizer):
         self.regularization = regularizer
 
+    def set_scaler(self, scaler):
+        self.scaler = scaler
+
     def forward(self, x):
         x = self.input_activation(x)
         
         out = self.layer(x)
         out = self.normalization(out)
+        out = self.scaler(out)
+
         out = self.activation(out)
         
         if self.training:
