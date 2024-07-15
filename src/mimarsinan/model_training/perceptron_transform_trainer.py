2da745dad4690dea7843abd0e3d6362a6e767b7d
dyigitpolat <dyigit96@gmail.com>
2024-07-15 15:16:40 +0000

    fix perceptron transform trainer

--- src/mimarsinan/model_training/perceptron_transform_trainer.py
+++ src/mimarsinan/model_training/perceptron_transform_trainer.py
@@ -1,16 +1,14 @@
 from mimarsinan.model_training.basic_trainer import BasicTrainer
+from mimarsinan.models.perceptron_mixer.perceptron import Perceptron
 
 import copy
 import torch
 import torch.nn as nn
+import functools
 
 def _copy_param(param_to, param_from):
-
     with torch.no_grad():
-        if len(param_to.shape) == 0:
-            param_to.data = param_from.data
-        else:
-            param_to.data[:] = param_from.data[:]
+        param_to.data.copy_(param_from.data.clone())
 
 class PerceptronTransformTrainer(BasicTrainer):
     def __init__(
@@ -19,43 +17,59 @@ class PerceptronTransformTrainer(BasicTrainer):
         super().__init__(model, device, data_provider_factory, loss_function)
         self.aux_model = copy.deepcopy(self.model).to(self.device)
         self.perceptron_transformation = perceptron_transformation
+
+    def _get_module_by_name(self, model, access_string):
+        names = access_string.split('.')
+        return functools.reduce(getattr, names, model)
     
     def _update_and_transform_model(self):
-        for perceptron, aux_perceptron in zip(self.model.get_perceptrons(), self.aux_model.get_perceptrons()):
-            # Transfer non-grad params
-            for aux_param, param in zip(aux_perceptron.parameters(), perceptron.parameters()):
-                if not aux_param.requires_grad:
-                    _copy_param(aux_param, param)
+        perceptron_ids = []
+        for name, module in self.model.named_modules():
+            if isinstance(module, Perceptron):
+                perceptron_ids.append(name)
+
+        aux_model_state_dict = copy.deepcopy(self.aux_model.state_dict())
+        model_state_dict = copy.deepcopy(self.model.state_dict())
+
+        for perceptron_id in perceptron_ids:
+            perceptron = self._get_module_by_name(self.model, perceptron_id)
+            aux_perceptron = self._get_module_by_name(self.aux_model, perceptron_id)
+
+            perceptron_param_ids = []
+            for name, _ in perceptron.named_parameters():
+                perceptron_param_ids.append(name)
 
             temp = copy.deepcopy(aux_perceptron).to(self.device)
-
-            if not isinstance(perceptron.normalization, nn.Identity):
-                temp.normalization.running_mean.data[:] = perceptron.normalization.running_mean.data[:]
-                temp.normalization.running_var.data[:] = perceptron.normalization.running_var.data[:]
+            #temp.load_state_dict(copy.deepcopy(aux_perceptron.state_dict()))
 
             self.perceptron_transformation(temp)
 
-            # Handle non-grad params
-            for aux_param, temp_param in zip(aux_perceptron.parameters(), temp.parameters()):
-                if not aux_param.requires_grad:
-                    _copy_param(aux_param, temp_param)
+            for perceptron_param_id in perceptron_param_ids:
+                model_state_dict[perceptron_id + '.' + perceptron_param_id].data.copy_(
+                    temp.state_dict()[perceptron_param_id].data.clone().detach())
+                
+        self.model.load_state_dict(copy.deepcopy(model_state_dict))
+        self.model = self.model.to(self.device)
 
-            for param, temp_param in zip(perceptron.parameters(), temp.parameters()):
-                _copy_param(param, temp_param)
 
     def _transfer_gradients_to_aux(self):
         for param, aux_param in zip(self.model.parameters(), self.aux_model.parameters()):
-            if param.requires_grad:
-                aux_param.grad = param.grad
+            if aux_param.requires_grad and param.grad is not None:
+                if aux_param.grad is None:
+                    aux_param.grad = param.grad.clone()
+                
+                aux_param.grad.copy_(param.grad.clone())
 
     def _backward_pass_on_loss(self, x, y, scaler):
-        self._update_and_transform_model()
+        with torch.no_grad():
+            self._update_and_transform_model()
         self.aux_model = self.aux_model.to(self.device)
         self.aux_model.train()
 
         loss = super()._backward_pass_on_loss(x, y, scaler)
 
-        self._transfer_gradients_to_aux()
+        with torch.no_grad():
+            self._transfer_gradients_to_aux()
         return loss
     
     def _get_optimizer_and_scheduler(self, lr, epochs):
@@ -66,4 +80,4 @@ class PerceptronTransformTrainer(BasicTrainer):
             optimizer, lr_lambda = lambda epoch: 1)
         scheduler = identity_scheduler
         
-        return optimizer, scheduler, torch.cuda.amp.GradScaler()
+        return optimizer, scheduler, torch.cuda.amp.GradScaler(enabled=False)
