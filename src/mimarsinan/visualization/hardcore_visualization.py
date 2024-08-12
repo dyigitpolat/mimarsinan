aba0674efe18f885c8038c6543fe4bd5fa89bfd4
dyigitpolat <dyigit96@gmail.com>
2024-08-12 16:46:14 +0000

    better hc visualization script

--- src/mimarsinan/visualization/hardcore_visualization.py
+++ src/mimarsinan/visualization/hardcore_visualization.py
@@ -1,5 +1,4 @@
 import matplotlib.pyplot as plt
-
 import numpy as np
 import math
 
@@ -8,16 +7,19 @@ class HardCoreMappingVisualizer:
         self.mapping = mapping
 
     def visualize(self, filename):
-        rows = int(math.sqrt(len(self.mapping.cores)))
-        cols = int(math.ceil(len(self.mapping.cores) / rows))
-        fig, axs = plt.subplots(nrows=rows, ncols=cols, figsize=(10, 10))
+        num_cores = len(self.mapping.cores)
+        cols = int(math.ceil(math.sqrt(num_cores)))
+        rows = int(math.ceil(num_cores / cols))
 
-        if rows == 1:
-            axs = [axs]  # Convert to a list of Axes objects
+        # Calculate the figure size based on the number of cores
+        fig_width = cols * 4  # 4 inches per core width
+        fig_height = rows * 4  # 4 inches per core height
+        fig, axs = plt.subplots(nrows=rows, ncols=cols, figsize=(fig_width, fig_height))
 
-        for i_y in range(rows):
-            for i_x in range(cols):
-                axs[i_y][i_x].axis('off')
+        if num_cores == 1:
+            axs = np.array([[axs]])  # Convert to a 2D array of Axes objects
+        elif rows == 1:
+            axs = axs.reshape(1, -1)  # Ensure axs is always a 2D array
 
         for i, core in enumerate(self.mapping.cores):
             i_y = i // cols
@@ -25,10 +27,14 @@ class HardCoreMappingVisualizer:
 
             axs[i_y][i_x].imshow(np.abs(core.core_matrix), cmap='YlOrRd')
             axs[i_y][i_x].set_title(f'Core {i+1}, L={core.latency}', fontsize=12)
+            axs[i_y][i_x].axis('off')
+
+        # Hide any unused subplots
+        for i in range(num_cores, rows * cols):
+            i_y = i // cols
+            i_x = i % cols
+            axs[i_y][i_x].axis('off')
 
         fig.tight_layout()
-
-        plt.savefig(filename)
+        plt.savefig(filename, dpi=100, bbox_inches='tight')
         plt.close()
-
-    
